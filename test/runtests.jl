2c57c0cf9da9b98cc37b1b578fb0445c74daf4b5
Dominique Orban <dominique.orban@gmail.com>
2015-10-12 20:45:13 -0400

    Tests.

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,10 @@
 using OptimizationProblems
 using Base.Test
 
-# write your own tests here
-@test 1 == 1
+# Test that every problem can be instantiated.
+for prob in names(OptimizationProblems)
+  prob == :OptimizationProblems && continue
+  println(prob)
+  prob_fn = eval(prob)
+  prob_fn()
+end
