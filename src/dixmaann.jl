53def370b1b3fe072d16dfcb7dcaf6a76890d30d
Dominique Orban <dominique.orban@gmail.com>
2015-10-12 22:32:48 -0400

    Remove redundant problems.

--- src/dixmaann.jl
+++
@@ -1,2 +0,0 @@
-"Dixon-Maany function in size `n` (version N)"
-dixmaann(n :: Int=99) = dixmaanm(n, α=1.0, β=0.0625, γ=0.0625, δ=0.0625)
--- src/dixmaano.jl
+++
@@ -1,2 +0,0 @@
-"Dixon-Maany function in size `n` (version O)"
-dixmaano(n :: Int=99) = dixmaanm(n, α=1.0, β=0.125, γ=0.125, δ=0.125)
