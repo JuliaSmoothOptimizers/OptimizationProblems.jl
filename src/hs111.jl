754cf88de35d068c2f99df105ca29d727a40086d
Nicole S Antunes <nicolesantunes@gmail.com>
2016-10-03 19:54:44 -0300

    Implementation of the models 111 and 112 from HS

---
+++ src/hs111.jl
@@ -0,0 +1,54 @@
+# Hock and Schittkowski problem number 111.
+#
+#   Source:
+#   Problem 111 in
+#   W. Hock and K. Schittkowski,
+#   Test examples for nonlinear programming codes,
+#   Lectures Notes in Economics and Mathematical Systems 187,
+#   Springer Verlag, Heidelberg, 1981.
+#
+#   classification QQR2-AN-2-1
+#
+# N. Antunes, Curitiba, 10/2016.
+
+export hs111
+
+"HS111 model"
+function hs111(args...)
+
+  nlp = Model()
+
+  for j=1:10
+    x0[j] = -2.3
+  end
+
+  c = [-6.089, -17.164, -34.054, -5.914, -24.721, -14.986, -24.100, -10.708, -26.662, -22.179]
+
+  @variable(nlp, x[i=1:10], start=x0[i])
+
+  @NLobjective(
+    nlp,
+    Min,
+    sum {exp(x[j])*(c[j] + x[j] - log(sum{exp(x[k]), k=1:10})), j=1:10}
+  )
+
+  @NLconstraint(
+    nlp,
+    constr,
+    exp(x[1]) + 2*exp(x[2]) + 2*exp(x[3]) + exp(x[6]) + exp(x[10]) - 2 == 0
+  )
+
+  @NLconstraint(
+    nlp,
+    constr,
+    exp(x[4]) + 2*exp(x[5]) + exp(x[6]) + exp(x[7]) - 1 == 0
+  )
+
+  @NLconstraint(
+    nlp,
+    constr,
+    exp(x[3]) + exp(x[7]) + exp(x[8]) + 2*exp(x[9]) + exp(x[10]) - 1 == 0
+  )
+
+  return nlp
+end
