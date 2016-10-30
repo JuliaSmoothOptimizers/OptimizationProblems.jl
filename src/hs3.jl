cf082b0b2dd03c783f59143084cb4dc7258c346f
Nicole S Antunes <nicolesantunes@gmail.com>
2016-10-30 17:17:09 -0200

    hs1-5, hs10-18, hs20: new models

---
+++ src/hs3.jl
@@ -0,0 +1,37 @@
+# Hock and Schittkowski problem number 3.
+#
+#   Source:
+#   Problem 3 in
+#   W. Hock and K. Schittkowski,
+#   Test examples for nonlinear programming codes,
+#   Lectures Notes in Economics and Mathematical Systems 187,
+#   Springer Verlag, Heidelberg, 1981.
+#
+#   classification QQR2-AN-2-1
+#
+# N. Antunes, Curitiba, 10/2016.
+
+export hs3
+
+"HS3 model"
+function hs3(args...)
+
+  nlp = Model()
+  x0 = [10, 1]
+
+  @variable(nlp, x[i=1:2], start=x0[i])
+
+  @NLobjective(
+    nlp,
+    Min,
+    x[2] + 0.00001*(x[2]-x[1])^2
+  )
+
+  @NLconstraint(
+    nlp,
+    constr,
+    0 <= x[2]
+  )
+
+  return nlp
+end
