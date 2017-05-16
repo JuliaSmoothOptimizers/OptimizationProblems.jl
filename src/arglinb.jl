60ee5d95f6d574c238ebcb86b6d55a117a6c0865
Dominique Orban <dominique.orban@gmail.com>
2017-05-15 20:10:58 -0400

    add a few convex models
    models implemented by @DahitoMA

---
+++ src/arglinb.jl
@@ -0,0 +1,31 @@
+#   Linear function - rank 1
+#
+#   Source: Problem 33 in
+#   J.J. More', B.S. Garbow and K.E. Hillstrom,
+#   "Testing Unconstrained Optimization Software",
+#   ACM Transactions on Mathematical Software, vol. 7(1), pp. 17-41, 1981.
+#
+#   See also Buckley#93 (with different N and M)
+#
+#   classification SUR2-AN-V-0
+
+export arglinb
+
+"Linear function with `n` parameters and `m` observations - rank 1"
+function arglinb(n::Int=10, m::Int=20)
+
+  m < n && warn("arglinb: must have m ≥ n")
+  m = max(m, n)
+
+  nlp = Model()
+
+  @variable(nlp, x[j=1:n], start=1.0)
+
+  @NLobjective(
+    nlp,
+    Min,
+    sum((i * sum(j * x[j] for j = 1:n) - 1)^2 for i = 1:m)
+  )
+
+  return nlp
+end
---
+++ src/meyer3.jl
@@ -0,0 +1,36 @@
+#   Meyer function
+#
+#   Source:  Problem 10 in
+#   J.J. More', B.S. Garbow and K.E. Hillstrom,
+#   "Testing Unconstrained Optimization Software",
+#   ACM Transactions on Mathematical Software, vol. 7(1), pp. 17-41, 1981.
+#
+#   See also Buckley #29 (p. 73).
+#
+#   classification SUR2-RN-3-0
+
+export meyer3
+
+"Meyer function"
+function meyer3()
+
+  nlp = Model()
+
+  x0 = [0.02, 4000, 250]
+
+  @variable(nlp, x[j=1:3], start=x0[j])
+
+  y = [34780, 28610, 23650, 19630, 16370, 13720, 11540, 9744, 8261, 7030, 6005,
+  5147, 4427, 3820, 3307, 2872]
+
+  t = 45 + 5 * (1:16)
+
+  @NLobjective(
+    nlp,
+    Min,
+    sum((x[1] * exp(x[2]/(t[i] + x[3])) - y[i])^2 for i = 1:16)
+  )
+
+  return nlp
+
+end
