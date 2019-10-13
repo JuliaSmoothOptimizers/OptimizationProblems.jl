423581b9e7d2415c12c638d475780e9f28acd4b8
Tim Holy <tim.holy@gmail.com>
2019-10-13 05:58:23 -0500

    Add endpoints and other fixes to morebv

--- src/morebv.jl
+++ src/morebv.jl
@@ -28,20 +28,20 @@ function morebv(n :: Int=100)
     n < 2 && @warn("morebv: number of variables must be ≥ 4")
     n = max(2, n)
 
-    h = 1.0/(n-1)
+    h = 1.0/(n+1)
 
     x0 = 0.5 * ones(n)
-    x0[1] = 0.0
-    x0[n] = 0.0
 
     nlp = Model()
 
-    @variable(nlp, x[i=1:n], start=x0[i])
+    @variable(nlp, x[i=1:n], start=x0[i])   # AMPL starts at i*h*(i*h - 1), ref https://github.com/mpf/Optimization-Test-Problems/blob/master/cute/morebv.mod
 
     @NLobjective(
       nlp,
       Min,
-	    sum((2.0 * x[i] - x[i-1] - x[i+1] + (h^2 / 2.0) * (x[i] + (i - 1) * h + 1)^3)^2 for i=2:n-1)
+        sum((2.0 * x[i] - x[i-1] - x[i+1] + (h^2 / 2.0) * (x[i] + i * h + 1)^3)^2 for i=2:n-1) +
+        (2.0 * x[1] - x[2] + (h^2 / 2.0) * (x[1] + 1)^3)^2 +
+        (2.0 * x[n] - x[n-1] + (h^2 / 2.0) * (x[n] + n * h + 1)^3)^2
     )
 
     return nlp
