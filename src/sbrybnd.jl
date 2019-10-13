8554d37438f19d76843afbdb8cd4d227e88f9839
Tim Holy <tim.holy@gmail.com>
2019-10-13 05:59:41 -0500

    Add notes on sbrybnd

--- src/sbrybnd.jl
+++ src/sbrybnd.jl
@@ -2,7 +2,7 @@
 #   least square sense.
 #   NB: scaled version of BRYBND
 
-#   Source: problem 31 in
+#   Source: the *unscaled* version is problem 31 in
 #   J.J. More', B.S. Garbow and K.E. Hillstrom,
 #   "Testing Unconstrained Optimization Software",
 #   ACM Transactions on Mathematical Software, vol. 7(1), pp. 17-41, 1981.
@@ -11,7 +11,7 @@
 
 #   classification SUR2-AN-V-0
 
-#   Problem 48 in
+#   This scaled version is Problem 48 in
 #   L. Luksan, C. Matonoha and J. Vlcek
 #   Modified CUTE problems for sparse unconstrained optimization,
 #   Technical Report 1081,
@@ -19,7 +19,8 @@
 #   Academy of Science of the Czech Republic
 #   http://www.cs.cas.cz/matonoha/download/V1081.pdf
 #
-# The terms in the sum should be squared --- corrected
+# The terms in the sum should be squared --- corrected (it is not squared in Luksan, but
+# comparing it against their brybnd that appears to be a typo)
 #
 # J.-P. Dussault, Clermont-Ferrand 05/2016.
 
