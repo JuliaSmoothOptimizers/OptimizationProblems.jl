33e0ec875507d2e2f5f8f249b8422d27e6b1b51a
Dominique Orban <dominique.orban@gmail.com>
2019-10-14 17:51:26 -0400

    hs55: fix index in constraint

--- src/hs55.jl
+++ src/hs55.jl
@@ -22,7 +22,7 @@ function hs55(args...)
   @variable(nlp, 0 <= x[i=1:6] <= uvar[i], start = x0[i])
 
   @constraint(nlp, x[1] + 2*x[2] + 5*x[5] - 6 == 0)
-  @constraint(nlp, x[3] + x[2] + x[3] - 3 == 0)
+  @constraint(nlp, x[1] + x[2] + x[3] - 3 == 0)
   @constraint(nlp, x[4] + x[5] + x[6] - 2 == 0)
   @constraint(nlp, x[1] + x[4] - 1 == 0)
   @constraint(nlp, x[2] + x[5] - 2 == 0)
