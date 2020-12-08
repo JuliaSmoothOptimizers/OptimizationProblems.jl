09e1722b267683b13b7f11ea2dea172f18f254f4
Alexis Montoison <alexis.montoison@polymtl.ca>
2020-12-08 11:56:33 -0500

    Remove template.jl

--- src/OptimizationProblems.jl
+++ src/OptimizationProblems.jl
@@ -3,10 +3,11 @@ module OptimizationProblems
 using JuMP
 
 path = dirname(@__FILE__)
-files = filter(x->x[end-2:end] == ".jl", readdir(path))
+files = filter(x -> x[end-2:end] == ".jl", readdir(path))
 for file in files
-  if (file == "OptimizationProblems.jl") || (file == "template.jl"); continue; end
-  include(file)
+  if file ≠ "OptimizationProblems.jl"
+    include(file)
+  end
 end
 
 end # module
