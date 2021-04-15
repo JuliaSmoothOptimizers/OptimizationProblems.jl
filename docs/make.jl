6d3e294c9738cdb8669b930510c4bf4f483378bb
JSOBot <69814847+JSOBot@users.noreply.github.com>
2021-04-14 21:35:05 -0400

    adding/updating file: docs/make.jl

---
+++ docs/make.jl
@@ -0,0 +1,15 @@
+using Documenter, OptimizationProblems
+
+makedocs(
+  modules = [OptimizationProblems],
+  doctest = true,
+  # linkcheck = true,
+  strict = true,
+  format = Documenter.HTML(assets = ["assets/style.css"], prettyurls = get(ENV, "CI", nothing) == "true"),
+  sitename = "OptimizationProblems.jl",
+  pages = Any["Home" => "index.md",
+              "Tutorial" => "tutorial.md",
+              "Reference" => "reference.md"]
+)
+
+deploydocs(repo = "github.com/JuliaSmoothOptimizers/OptimizationProblems.jl.git")
