3db9493ca896ff72dde8274714afcabbb73eb796
Abel Soares Siqueira <abel.s.siqueira@gmail.com>
2021-07-10 10:44:00 -0300

    :robot: Fix docs to use main

--- docs/make.jl
+++ docs/make.jl
@@ -13,4 +13,4 @@ makedocs(
   pages = Any["Home" => "index.md", "Tutorial" => "tutorial.md", "Reference" => "reference.md"],
 )
 
-deploydocs(repo = "github.com/JuliaSmoothOptimizers/OptimizationProblems.jl.git")
+deploydocs(repo = "github.com/JuliaSmoothOptimizers/OptimizationProblems.jl.git", devbranch = "main")
