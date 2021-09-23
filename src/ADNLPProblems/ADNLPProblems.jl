b041ad193651bafef2f4db8cf25b29436017684c
tmigot <tangi.migot@gmail.com>
2021-09-23 17:03:47 +0200

    rmv `problems` entry

--- src/ADNLPProblems/ADNLPProblems.jl
+++ src/ADNLPProblems/ADNLPProblems.jl
@@ -3,7 +3,6 @@ module ADNLPProblems
 using Requires
 
 default_nvar = 100
-problems = []
 
 @init begin
   @require ADNLPModels = "54578032-b7ea-4c30-94aa-7cbd1cce6c9a" begin
@@ -14,8 +13,6 @@ problems = []
     for file in files
       if file ≠ "ADNLPProblems.jl"
         include(file)
-        name = split(file, ".")[1]
-        push!(problems, name)
       end
     end
   end
