353e46a9f6dc4c00e857112128bd64a63577f587
tmigot <tangi.migot@gmail.com>
2021-11-01 20:10:50 +0100

    include meta files

--- src/OptimizationProblems.jl
+++ src/OptimizationProblems.jl
@@ -5,4 +5,9 @@ include("PureJuMP/PureJuMP.jl")
 
 export PureJuMP, ADNLPProblems
 
+files = filter(x -> x[(end - 2):end] == ".jl", readdir("src/Meta"))
+for file in files, i = 1:number_of_problems
+  include("src/Meta/" * file)
+end
+
 end # module
