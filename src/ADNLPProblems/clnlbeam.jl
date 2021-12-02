1557a793e169969f5475c54ad7f67a81964b911b
tmigot <tangi.migot@gmail.com>
2021-12-02 16:10:46 +0100

    add clnlbeam problem
    add lin

---
+++ src/ADNLPProblems/clnlbeam.jl
@@ -0,0 +1,36 @@
+export clnlbeam
+
+function clnlbeam(args...; n::Int = default_nvar, type::Val{T} = Val(Float64), kwargs...) where {T}
+  N = div(n - 3, 3)
+  h = one(T) / N
+  alpha = 350
+  function f(y)
+    t, x, u = y[1:(N + 1)], y[(N + 2):(2 * N + 2)], y[(2 * N + 3):end]
+    return sum(
+      T(0.5) * h * (u[i + 1]^2 + u[i]^2) + T(0.5) * alpha * h * (cos(t[i + 1]) + cos(t[i])) for
+      i = 1:N
+    )
+  end
+  function c(y)
+    t, x, u = y[1:(N + 1)], y[(N + 2):(2 * N + 2)], y[(2 * N + 3):end]
+    return vcat(
+      [x[i + 1] - x[i] - T(0.5) * h * (sin(t[i + 1]) + sin(t[i])) for i = 1:N],
+      [t[i + 1] - t[i] - T(0.5) * h * u[i + 1] - T(0.5) * h * u[i] for i = 1:N],
+    )
+  end
+  lvar = vcat(-ones(T, N + 1), -T(0.05) * ones(T, N + 1), -T(Inf) * ones(T, N + 1))
+  uvar = vcat(ones(T, N + 1), T(0.05) * ones(T, N + 1), T(Inf) * ones(T, N + 1))
+  xi = zeros(T, 3 * N + 3)
+  return ADNLPModels.ADNLPModel(
+    f,
+    xi,
+    lvar,
+    uvar,
+    c,
+    zeros(T, 2 * N),
+    zeros(T, 2 * N),
+    lin = 1:N,
+    name = "clnlbeam";
+    kwargs...,
+  )
+end
---
+++ src/Meta/clnlbeam.jl
@@ -0,0 +1,25 @@
+clnlbeam_meta = Dict(
+  :nvar => 99,
+  :variable_nvar => true,
+  :ncon => 64,
+  :variable_ncon => true,
+  :minimize => true,
+  :name => "clnlbeam",
+  :has_equalities_only => true,
+  :has_inequalities_only => false,
+  :has_bounds => true,
+  :has_fixed_variables => false,
+  :objtype => :other,
+  :contype => :general,
+  :best_known_lower_bound => -Inf,
+  :best_known_upper_bound => 350.0,
+  :is_feasible => true,
+  :defined_everywhere => missing,
+  :origin => :unknown,
+)
+get_clnlbeam_nvar(; n::Integer = default_nvar, kwargs...) = 3 * div(n - 3, 3) + 3
+get_clnlbeam_ncon(; n::Integer = default_nvar, kwargs...) = 2 * div(n - 3, 3)
+get_clnlbeam_nlin(; n::Integer = default_nvar, kwargs...) = 0
+get_clnlbeam_nnln(; n::Integer = default_nvar, kwargs...) = 2 * div(n - 3, 3)
+get_clnlbeam_nequ(; n::Integer = default_nvar, kwargs...) = 2 * div(n - 3, 3)
+get_clnlbeam_nineq(; n::Integer = default_nvar, kwargs...) = 0
