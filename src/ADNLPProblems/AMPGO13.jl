a7da8d482a433ada9a16220d5d40f41f12d3c34b
Dominique <dominique.orban@gmail.com>
2021-12-13 12:11:52 -0500

    Update AMPGO13.jl
    fix #100

--- src/ADNLPProblems/AMPGO13.jl
+++ src/ADNLPProblems/AMPGO13.jl
@@ -3,8 +3,8 @@ export AMPGO13
 function AMPGO13(; n::Int = default_nvar, type::Val{T} = Val(Float64), kwargs...) where {T}
   function f(x)
     n = length(x)
-    return 0.0 < x[1] < 1.0 ? -T(x[1]^0.666666 + (1 - x[1]^2)^0.333333) : Inf
+    return 0 < x[1] < 1 ? -T(x[1]^(2/3) + (1 - x[1]^2)^(1/3)) : T[Inf]
   end
-  x0 = T[0.001]
+  x0 = T[1/1000]
   return ADNLPModels.ADNLPModel(f, x0, name = "AMPGO13"; kwargs...)
 end
