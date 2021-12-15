7cbd1b830836a2aa3d82d452f7fae0caf93dbad7
tmigot <tangi.migot@gmail.com>
2021-12-14 19:23:13 -0500

    vectorized matrix unknown

--- src/ADNLPProblems/fminsrf2.jl
+++ src/ADNLPProblems/fminsrf2.jl
@@ -8,9 +8,9 @@ function fminsrf2(; n::Int = default_nvar, type::Val{T} = Val(Float64), kwargs..
   p * p != n && @warn("fminsrf2: number of variables adjusted from $n down to $(p*p)")
   n = p * p
 
-  h00 = 1.0
-  slopej = 4.0
-  slopei = 8.0
+  h00 = one(T)
+  slopej = T(4)
+  slopei = T(8)
 
   scale = (p - 1)^2
 
@@ -20,23 +20,26 @@ function fminsrf2(; n::Int = default_nvar, type::Val{T} = Val(Float64), kwargs..
   h10 = h00 + slopei
   mid = div(p, 2)
 
-  x0 = zeros(p, p)
+  x0 = zeros(T, p * p)
   I = 2:(p - 1)
   J = 1:p
 
-  x0[I, 1] .= (I .- 1) * ston .+ h01
-  x0[I, p] .= (I .- 1) * ston .+ h00
-  x0[1, J] .= (J .- 1) * wtoe .+ h00
-  x0[p, J] .= (J .- 1) * wtoe .+ h10
+  x0[I] .= (I .- 1) * ston .+ h01
+  x0[I .+ (p .- 1) * p] .= (I .- 1) * ston .+ h00
+  x0[1 .+ (J .- 1) * p] .= (J .- 1) * wtoe .+ h00
+  x0[p .+ (J .- 1) * p] .= (J .- 1) * wtoe .+ h10
   function f(x)
     n = length(x)
     return sum(
       sum(
-        100.0 * sqrt(
-          0.5 * (p - 1)^2 * ((x[i, j] - x[i + 1, j + 1])^2 + (x[i + 1, j] - x[i, j + 1])^2) + 1.0,
+        100 * sqrt(
+          T(1 / 2) *
+          (p - 1)^2 *
+          ((x[i + (j - 1) * p] - x[i + 1 + j * p])^2 + (x[i + 1 + (j - 1) * p] - x[i + j * p])^2) +
+          1,
         ) / scale for i = 1:(p - 1)
       ) for j = 1:(p - 1)
-    ) + 100.0 * (x[mid, mid])^2 / n
+    ) + 100 * (x[mid + (mid - 1) * p])^2 / n
   end
   return ADNLPModels.ADNLPModel(f, x0, name = "fminsrf2"; kwargs...)
 end
