300d145b407ef89e24753cd29ee8b1b5ea524d96
tmigot <tangi.migot@gmail.com>
2022-03-02 09:24:37 -0500

    remove `T` to convert vector

--- src/ADNLPProblems/AMPGO13.jl
+++ src/ADNLPProblems/AMPGO13.jl
@@ -3,7 +3,7 @@ export AMPGO13
 function AMPGO13(; n::Int = default_nvar, type::Val{T} = Val(Float64), kwargs...) where {T}
   function f(x)
     n = length(x)
-    return 0 < x[1] < 1 ? -T(x[1]^(2 / 3) + (1 - x[1]^2)^(1 / 3)) : T[Inf]
+    return 0 < x[1] < 1 ? -(x[1]^T(2 / 3) + (1 - x[1]^2)^T(1 / 3)) : T[Inf]
   end
   x0 = T[1 / 1000]
   return ADNLPModels.ADNLPModel(f, x0, name = "AMPGO13"; kwargs...)
--- src/ADNLPProblems/hs11.jl
+++ src/ADNLPProblems/hs11.jl
@@ -3,7 +3,7 @@ export hs11
 function hs11(; n::Int = default_nvar, type::Val{T} = Val(Float64), kwargs...) where {T}
   x0 = T[4.9; 0.1]
   f(x) = (x[1] - 5)^2 + x[2]^2 - 25
-  c(x) = T[x[1]^2 - x[2]]
+  c(x) = [x[1]^2 - x[2]]
   lcon = -T[Inf]
   ucon = T[0.0]
 
