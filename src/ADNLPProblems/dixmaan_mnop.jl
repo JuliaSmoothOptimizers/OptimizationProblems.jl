cd0edec48128c979f2b685c35428ac534b069ab9
d-monnet <70266099+d-monnet@users.noreply.github.com>
2022-03-24 17:31:10 -0400

    Remove underscore to dixmaan name

--- src/ADNLPProblems/dixmaan_mnop.jl
+++ src/ADNLPProblems/dixmaan_mnop.jl
@@ -21,14 +21,14 @@ function dixmaanm(;
            sum(T(i / n)^2 * T(δ) * x[i] * x[i + 2 * m] for i = 1:m)
   end
   x0 = 2 * ones(T, n)
-  return ADNLPModels.ADNLPModel(f, x0, name = "dixmaan_m"; kwargs...)
+  return ADNLPModels.ADNLPModel(f, x0, name = "dixmaanm"; kwargs...)
 end
 
 dixmaann(; n::Int = default_nvar, type::Val{T} = Val(Float64), kwargs...) where {T} =
-  dixmaanm(n = n, type = type, α = one(T), β = T(0.0625), γ = T(0.0625), δ = T(0.0625), name = "dixmaan_n"; kwargs...)
+  dixmaanm(n = n, type = type, α = one(T), β = T(0.0625), γ = T(0.0625), δ = T(0.0625), name = "dixmaann"; kwargs...)
 
 dixmaano(; n::Int = default_nvar, type::Val{T} = Val(Float64), kwargs...) where {T} =
-  dixmaanm(n = n, type = type, α = one(T), β = T(0.125), γ = T(0.125), δ = T(0.125), name = "dixmaan_o"; kwargs...)
+  dixmaanm(n = n, type = type, α = one(T), β = T(0.125), γ = T(0.125), δ = T(0.125), name = "dixmaano"; kwargs...)
 
 dixmaanp(; n::Int = default_nvar, type::Val{T} = Val(Float64), kwargs...) where {T} =
-  dixmaanm(n = n, type = type, α = one(T), β = T(0.26), γ = T(0.26), δ = T(0.26), name = "dixmaan_p"; kwargs...)
+  dixmaanm(n = n, type = type, α = one(T), β = T(0.26), γ = T(0.26), δ = T(0.26), name = "dixmaanp"; kwargs...)
