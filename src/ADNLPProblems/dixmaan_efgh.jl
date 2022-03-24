0b84478dc794d06806b63c05a437919672f18a92
d-monnet <70266099+d-monnet@users.noreply.github.com>
2022-03-24 10:34:20 -0400

    Fix dixmaan efgh name

--- src/ADNLPProblems/dixmaan_efgh.jl
+++ src/ADNLPProblems/dixmaan_efgh.jl
@@ -21,14 +21,14 @@ function dixmaane(;
            sum(T(i / n) * T(δ) * x[i] * x[i + 2 * m] for i = 1:m)
   end
   x0 = 2 * ones(T, n)
-  return ADNLPModels.ADNLPModel(f, x0, name = "dixmaan_efgh"; kwargs...)
+  return ADNLPModels.ADNLPModel(f, x0, name = "dixmaan_e"; kwargs...)
 end
 
 dixmaanf(; n::Int = default_nvar, type::Val{T} = Val(Float64), kwargs...) where {T} =
-  dixmaane(n = n, type = type, α = one(T), β = T(0.0625), γ = T(0.0625), δ = T(0.0625); kwargs...)
+  dixmaane(n = n, type = type, α = one(T), β = T(0.0625), γ = T(0.0625), δ = T(0.0625), name = "dixmaan_f"; kwargs...)
 
 dixmaang(; n::Int = default_nvar, type::Val{T} = Val(Float64), kwargs...) where {T} =
-  dixmaane(n = n, type = type, α = one(T), β = T(0.125), γ = T(0.125), δ = T(0.125); kwargs...)
+  dixmaane(n = n, type = type, α = one(T), β = T(0.125), γ = T(0.125), δ = T(0.125), name = "dixmaan_g"; kwargs...)
 
 dixmaanh(; n::Int = default_nvar, type::Val{T} = Val(Float64), kwargs...) where {T} =
-  dixmaane(n = n, type = type, α = one(T), β = T(0.26), γ = T(0.26), δ = T(0.26); kwargs...)
+  dixmaane(n = n, type = type, α = one(T), β = T(0.26), γ = T(0.26), δ = T(0.26), name = "dixmaan_h"; kwargs...)
