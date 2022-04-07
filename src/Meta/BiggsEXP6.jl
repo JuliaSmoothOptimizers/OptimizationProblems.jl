1fdee9af1107935e7351c5513303a6a7ee3b1f5c
Jean-Charles LAYOUN <jean-charles.layoun@polytechnique.edu>
2022-04-07 14:24:36 -0400

    Added meta file for BiggsEXP6

---
+++ src/Meta/BiggsEXP6.jl
@@ -0,0 +1,8 @@
+BiggsEXP6 = Dict()
+
+get_BiggsEXP6_nvar(; kwargs...) = 6
+get_AMPGO03_ncon(; kwargs...) = 0
+get_AMPGO03_nlin(; kwargs...) = 0
+get_AMPGO03_nnln(; kwargs...) = 0
+get_AMPGO03_nequ(; kwargs...) = 0
+get_AMPGO03_nineq(; kwargs...) = 0
