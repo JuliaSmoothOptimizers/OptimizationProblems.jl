659dc9214e71e4309916d7c78fc620711d940551
Samuel Mendoza <89709335+Smendoza98@users.noreply.github.com>
2022-04-11 18:51:00 -0400

    Add jennrichsampson problem (#134)
    * Add jennrichsampson problem

---
+++ src/Meta/jennrichsampson.jl
@@ -0,0 +1,25 @@
+jennrichsampson_meta = Dict(
+:nvar => 2,
+:variable_nvar => false,
+:ncon => 0,
+:variable_ncon => false,
+:minimize => true,
+:name => "jennrichsampson",
+:has_equalities_only => false,
+:has_inequalities_only => false,
+:has_bounds => false,
+:has_fixed_variables => false,
+:objtype => :other,
+:contype => :unconstrained,
+:best_known_lower_bound => -Inf,
+:best_known_upper_bound => 4171.306161960493,
+:is_feasible => true,
+:defined_everywhere => missing,
+:origin => :unknown,
+)
+get_jennrichsampson_nvar(; n::Integer = default_nvar, kwargs...) = 2
+get_jennrichsampson_ncon(; n::Integer = default_nvar, kwargs...) = 0
+get_jennrichsampson_nlin(; n::Integer = default_nvar, kwargs...) = 0
+get_jennrichsampson_nnln(; n::Integer = default_nvar, kwargs...) = 0
+get_jennrichsampson_nequ(; n::Integer = default_nvar, kwargs...) = 0
+get_jennrichsampson_nineq(; n::Integer = default_nvar, kwargs...) = 0
---
+++ src/PureJuMP/jennrichsampson.jl
@@ -0,0 +1,19 @@
+#   Source: problem 6 in
+#   J.J. More', B.S. Garbow and K.E. Hillstrom,
+#   "Testing Unconstrained Optimization Software",
+#   ACM Transactions on Mathematical Software, vol. 7(1), pp. 17-41, 1981.
+
+export jennrichsampson
+
+function jennrichsampson(args...; n::Int = default_nvar, m::Int = 10, kwargs...)
+
+    nlp = Model()
+    @variable(nlp, x1)
+    set_start_value(x1, 0.3)
+    @variable(nlp, x2)
+    set_start_value(x2, 0.4)
+    @NLobjective(nlp, Min, sum((2 + 2*i - (exp(i*x1) + exp(i*x2)))^2 for i in 1:m))
+
+    return nlp
+end
+
