74e9ca07413598d1e39700c9a14c7ea66251652f
tmigot <tmigot@users.noreply.github.com>
2022-04-11 22:52:26 +0000

    :robot: Format .jl files

--- src/ADNLPProblems/jennrichsampson.jl
+++ src/ADNLPProblems/jennrichsampson.jl
@@ -1,9 +1,13 @@
 export jennrichsampson
 
-function jennrichsampson(; n::Int = default_nvar, type::Val{T} = Val(Float64), m::Int=10, kwargs...) where {T}
+function jennrichsampson(;
+  n::Int = default_nvar,
+  type::Val{T} = Val(Float64),
+  m::Int = 10,
+  kwargs...,
+) where {T}
+  x0 = T[0.3; 0.4]
+  f(x) = sum((2 + 2 * i - (exp(i * x[1]) + exp(i * x[2])))^2 for i = 1:m)
 
-    x0 = T[0.3;0.4]
-    f(x) = sum((2 + 2*i - (exp(i*x[1]) + exp(i*x[2])))^2 for i in 1:m)
-
-    return ADNLPModels.ADNLPModel(f, x0, name = "jennrichsampson"; kwargs...)
+  return ADNLPModels.ADNLPModel(f, x0, name = "jennrichsampson"; kwargs...)
 end
