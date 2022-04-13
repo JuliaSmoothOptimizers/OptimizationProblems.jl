7afd6b10314233f71c93aae57d1936b4070682a2
xgaumir <98101400+xgaumir@users.noreply.github.com>
2022-04-12 20:04:31 -0400

    Update powellbs.jl

--- src/PureJuMP/powellbs.jl
+++ src/PureJuMP/powellbs.jl
@@ -10,7 +10,7 @@
 export powellbs
 
 function powellbs(args...; kwargs...)
-    nlp = model()
+    nlp = Model()
     @variable(nlp, x1, start=0)
     @variable(nlp, x2, start=1)
     @NLobjective(nlp, Min, 1/2*((10^(4)*x1*x2-1)^2+(exp(-x1)+exp(-x2)-1.0001)^2))
