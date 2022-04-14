140c9ddd4e62d95909e0bd2aa612596e00422a95
Sandra Parent <sandra.parent@polymtl.ca>
2022-04-14 15:14:58 -0400

    correction on ADNLPProblem for BOX3

--- src/ADNLPProblems/BOX3.jl
+++ src/ADNLPProblems/BOX3.jl
@@ -5,7 +5,7 @@ function BOX3(args...; n::Int = default_nvar, type::Val{T} = Val(Float64), m::In
   m = max(m, 3)
 
   x0 = T[0; 10; 20]
-  f(x) = one(T)/2*sum((exp(-one(T)/10*j*x[1]) - exp(-one(T)/10*j*x[2]) -x[3]*(exp(-one(T)/10*j) - exp(-j)))^2 for j = 1:m)
+  f(x) = one(T)/2*sum((exp(-one(T)/10*j*x[1]) - exp(-one(T)/10*j*x[2]) -x[3]*(exp(-one(T)/10*j) - exp(-T(j))))^2 for j = 1:m)
 
   return ADNLPModels.ADNLPModel(f, x0, name = "BOX3"; kwargs...)
 end
