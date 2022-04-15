83b7453718e2861dd2f59f009492a71b5057a57e
btoutee <bertrand.toutee@gmail.com>
2022-04-14 20:43:36 -0400

    Shortened name to kowosb, added meta and jump model and removed gaussian models that were not supposed to be part of this branch

---
+++ src/Meta/kowosb.jl
@@ -0,0 +1,25 @@
+kowosb_meta = Dict(
+  :nvar => 4,
+  :variable_nvar => false,
+  :ncon => 0,
+  :variable_ncon => false,
+  :minimize => true,
+  :name => "kowosb",
+  :has_equalities_only => false,
+  :has_inequalities_only => false,
+  :has_bounds => false,
+  :has_fixed_variables => false,
+  :objtype => :other,
+  :contype => :unconstrained,
+  :best_known_lower_bound => -Inf,
+  :best_known_upper_bound => 0.026497849149796696,
+  :is_feasible => true,
+  :defined_everywhere => missing,
+  :origin => :unknown,
+)
+get_kowosb_nvar(; n::Integer = default_nvar, kwargs...) = 4
+get_kowosb_ncon(; n::Integer = default_nvar, kwargs...) = 0
+get_kowosb_nlin(; n::Integer = default_nvar, kwargs...) = 0
+get_kowosb_nnln(; n::Integer = default_nvar, kwargs...) = 0
+get_kowosb_nequ(; n::Integer = default_nvar, kwargs...) = 0
+get_kowosb_nineq(; n::Integer = default_nvar, kwargs...) = 0
---
+++ src/PureJuMP/kowosb.jl
@@ -0,0 +1,21 @@
+#   Source:  Problem 15 in
+#   J.J. More', B.S. Garbow and K.E. Hillstrom,
+#   "Testing Unconstrained Optimization Software",
+#   ACM Transactions on Mathematical Software, vol. 7(1), pp. 17-41, 1981.
+
+#   classification SUR2-MN-4-0
+export kowosb
+
+function kowosb(args...; n::Int = default_nvar, kwargs...)
+  nlp = Model()
+
+  x0 = [0.25, 0.39, 0.415, 0.39]
+  @variable(nlp, x[i=1:4], start=x0[i])
+
+  m = 11
+  y = [0.1957, 0.1947, 0.1735, 0.1600, 0.0844, 0.0627, 0.0456, 0.0342, 0.0323, 0.0235, 0.0246]
+  u = [4, 2, 1, 0.5, 0.25, 0.167, 0.125, 0.1, 0.833, 0.0714, 0.0625]
+
+  @NLobjective(nlp, Min, sum((y[i] - (x[1]*(u[i]^2 + u[i]*x[2]))/(u[i]^2 + u[i]*x[3] + x[4]))^2 for i = 1:m))
+  return nlp
+end
