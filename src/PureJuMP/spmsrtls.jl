0cbaa545e16417b45859a852aaf3b943060041d7
tmigot <tangi.migot@gmail.com>
2022-04-15 17:50:29 +0200

    update doc

--- src/PureJuMP/spmsrtls.jl
+++ src/PureJuMP/spmsrtls.jl
@@ -6,6 +6,17 @@
 #   TR 1989CS-3, Mathematics, statistics and computing centre,
 #   Dalhousie University, Halifax (CDN), 1989.
 
+#   See also
+#
+#   problem 54 in
+#   L. Luksan, C. Matonoha and J. Vlcek
+#   Modified CUTE problems for sparse unconstrained optimization,
+#   Technical Report 1081,
+#   Institute of Computer Science,
+#   Academy of Science of the Czech Republic
+#
+#   http://www.cs.cas.cz/matonoha/download/V1081.pdf
+
 #   classification SUR2-AN-V-V
 export spmsrtls
 
