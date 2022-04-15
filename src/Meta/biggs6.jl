9863bff02b61ca24d5da4edba308580ec8bd76ea
Learningchipmunk <47299801+Learningchipmunk@users.noreply.github.com>
2022-04-15 19:50:12 -0400

    Biggs exp6 (#136)
    * Add BiggsEXP6

---
+++ src/Meta/biggs6.jl
@@ -0,0 +1,25 @@
+biggs6_meta = Dict(
+  :nvar => 6,
+  :variable_nvar => false,
+  :ncon => 0,
+  :variable_ncon => false,
+  :minimize => true,
+  :name => "biggs6",
+  :has_equalities_only => false,
+  :has_inequalities_only => false,
+  :has_bounds => false,
+  :has_fixed_variables => false,
+  :objtype => :other,
+  :contype => :unconstrained,
+  :best_known_lower_bound => -Inf,
+  :best_known_upper_bound => -3.156207810643302,
+  :is_feasible => true,
+  :defined_everywhere => missing,
+  :origin => :unknown,
+)
+get_biggs6_nvar(; n::Integer = default_nvar, kwargs...) = 6
+get_biggs6_ncon(; n::Integer = default_nvar, kwargs...) = 0
+get_biggs6_nlin(; n::Integer = default_nvar, kwargs...) = 0
+get_biggs6_nnln(; n::Integer = default_nvar, kwargs...) = 0
+get_biggs6_nequ(; n::Integer = default_nvar, kwargs...) = 0
+get_biggs6_nineq(; n::Integer = default_nvar, kwargs...) = 0
