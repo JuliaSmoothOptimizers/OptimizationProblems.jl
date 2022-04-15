e1d96dcfe0dd56fc6a1b41d571190909ffd6f216
The-Wolf3 <thewolf@gaming.cx>
2022-04-15 19:52:10 -0400

    Added Gaussian fct (#143)

---
+++ src/Meta/gaussian.jl
@@ -0,0 +1,25 @@
+gaussian_meta = Dict(
+  :nvar => 3,
+  :variable_nvar => false,
+  :ncon => 0,
+  :variable_ncon => false,
+  :minimize => true,
+  :name => "gaussian",
+  :has_equalities_only => false,
+  :has_inequalities_only => false,
+  :has_bounds => false,
+  :has_fixed_variables => false,
+  :objtype => :other,
+  :contype => :unconstrained,
+  :best_known_lower_bound => -Inf,
+  :best_known_upper_bound => 3.888106991166885e-6,
+  :is_feasible => true,
+  :defined_everywhere => missing,
+  :origin => :unknown,
+)
+get_gaussian_nvar(; n::Integer = default_nvar, kwargs...) = 3
+get_gaussian_ncon(; n::Integer = default_nvar, kwargs...) = 0
+get_gaussian_nlin(; n::Integer = default_nvar, kwargs...) = 0
+get_gaussian_nnln(; n::Integer = default_nvar, kwargs...) = 0
+get_gaussian_nequ(; n::Integer = default_nvar, kwargs...) = 0
+get_gaussian_nineq(; n::Integer = default_nvar, kwargs...) = 0
