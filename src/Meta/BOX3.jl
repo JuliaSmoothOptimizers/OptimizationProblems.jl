c49acaae5ad480f15893116b849f5cf07c82d99f
SandraParent <98102302+SandraParent@users.noreply.github.com>
2022-04-15 19:50:37 -0400

    BOX3 (#140)
    * add of box3 problem

---
+++ src/Meta/BOX3.jl
@@ -0,0 +1,25 @@
+BOX3_meta = Dict(
+  :nvar => 3,
+  :variable_nvar => false,
+  :ncon => 0,
+  :variable_ncon => false,
+  :minimize => true,
+  :name => "BOX3",
+  :has_equalities_only => false,
+  :has_inequalities_only => false,
+  :has_bounds => false,
+  :has_fixed_variables => false,
+  :objtype => :other,
+  :contype => :unconstrained,
+  :best_known_lower_bound => -Inf,
+  :best_known_upper_bound => 662.8684162588787,
+  :is_feasible => true,
+  :defined_everywhere => missing,
+  :origin => :unknown,
+)
+get_BOX3_nvar(; n::Integer = default_nvar, kwargs...) = 3
+get_BOX3_ncon(; n::Integer = default_nvar, kwargs...) = 0
+get_BOX3_nlin(; n::Integer = default_nvar, kwargs...) = 0
+get_BOX3_nnln(; n::Integer = default_nvar, kwargs...) = 0
+get_BOX3_nequ(; n::Integer = default_nvar, kwargs...) = 0
+get_BOX3_nineq(; n::Integer = default_nvar, kwargs...) = 0
