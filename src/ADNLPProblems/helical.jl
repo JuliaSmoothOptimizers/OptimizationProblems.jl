98c38e7aaaf6ffbb4a41fe4bc14ba021f9c79804
ines1406 <Condeines4@gmail.com>
2022-04-15 22:24:59 -0400

    Ajout des trois fichiers helical.jl

---
+++ src/ADNLPProblems/helical.jl
@@ -0,0 +1,9 @@
+export helical
+
+function helical(; n::Int = default_nvar, type::Val{T} = Val(Float64), kwargs...) where {T}
+  function f(x)
+    return (10*(x[3]-10*(atan(x[2]/x[1])-one(T)/4*(x[1]-abs(x[1])/x[1]))/(2*T(pi))))^2+(10*(sqrt(x[1]^2+x[2]^2)-1))^2+x[3]^2
+  end
+  x0 = T[-1; 0; 0]
+  return ADNLPModels.ADNLPModel(f, x0, name = "helical"; kwargs...)
+end
---
+++ src/PureJuMP/helical.jl
@@ -0,0 +1,14 @@
+#   Source: problem 7 in
+#   J.J. More', B.S. Garbow and K.E. Hillstrom,
+#   "Testing Unconstrained Optimization Software",
+#   ACM Transactions on Mathematical Software, vol. 7(1), pp. 17-41, 1981.
+
+export helical
+
+function helical()
+  nlp = Model()
+  x0 = [-1.0; 0.0;0.0]
+  @variable(nlp,x[i=1:3], start=x0[i])
+  @NLobjective(nlp,Min,(10*(x[3]-10*(atan(x[2]/x[1])-0.25*(x[1]-abs(x[1])/x[1]))/(2*pi)))^2+(10*(sqrt(x[1]^2+x[2]^2)-1))^2+x[3]^2)
+  return nlp
+end
