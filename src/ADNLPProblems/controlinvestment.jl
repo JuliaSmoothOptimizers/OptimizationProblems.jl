e2e0fcb491c17c91d1a3eae12581714d5b111778
tmigot <tangi.migot@gmail.com>
2022-04-18 16:35:03 +0200

    add `controlinvestment`

---
+++ src/ADNLPProblems/controlinvestment.jl
@@ -0,0 +1,33 @@
+export controlinvestment
+
+function controlinvestment(args...; n::Int = default_nvar, type::Val{T} = Val(Float64), kwargs...) where {T}
+  N = div(n, 2)
+  h = T(1 / N)
+  x0 = 1
+  gamma = 3
+  function f(y)
+    x, u = y[1:N], y[(N + 1):end]
+    return T(0.5) * h * sum((u[k] - 1) * x[k] + (u[k + 1] - 1) * x[k + 1] for k = 1:(N - 1))
+  end
+  function c(y)
+    x, u = y[1:N], y[(N + 1):end]
+    return [
+      x[k + 1] - x[k] - T(0.5) * h * gamma * (u[k] * x[k] + u[k + 1] * x[k + 1]) for k = 1:(N - 1)
+    ]
+  end
+  lvar = vcat(x0, -T(Inf) * ones(T, N - 1), zeros(T, N))
+  uvar = vcat(x0, T(Inf) * ones(T, N - 1), ones(T, N))
+  xi = vcat(ones(T, N), zeros(T, N))
+  return ADNLPModels.ADNLPModel(
+    f,
+    xi,
+    lvar,
+    uvar,
+    c,
+    zeros(T, N - 1),
+    zeros(T, N - 1),
+    name = "controlinvestment",
+    ;
+    kwargs...,
+  )
+end 
---
+++ src/PureJuMP/controlinvestment.jl
@@ -0,0 +1,25 @@
+export controlinvestment
+
+function controlinvestment(args...; n::Int = default_nvar, kwargs...)
+  nlp = Model()
+
+  N = div(n, 2)
+  h = 1 / N
+  x0 = 1.0
+  gamma = 3
+
+  @variable(nlp, x[1:N], start = x0)
+  @variable(nlp, 0 ≤ u[1:N] ≤ 1, start = 0)
+
+  @NLexpression(nlp, f[k = 1:N], (u[k] - 1) * x[k])
+  @NLobjective(nlp, Min, 0.5 * h * sum(f[k] + f[k + 1] for k = 1:(N - 1)))
+
+  @constraint(
+    nlp,
+    dx[k = 1:(N - 1)],
+    x[k + 1] - x[k] == 0.5 * h * gamma * (u[k] * x[k] + u[k + 1] * x[k + 1])
+  )
+  @constraint(nlp, x[1] == x0)
+
+  return nlp
+end
