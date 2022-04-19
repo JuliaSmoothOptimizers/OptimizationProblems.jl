1ca8bdcef73d3f584fb4000fff20e67268080a88
tmigot <tangi.migot@gmail.com>
2022-04-19 22:07:24 +0200

    fix meta

--- src/Meta/polygon1.jl
+++ src/Meta/polygon1.jl
@@ -17,9 +17,9 @@ polygon1_meta = Dict(
   :defined_everywhere => missing,
   :origin => :unknown,
 )
-get_polygon1_nvar(; n::Integer = default_nvar, kwargs...) = 2 * div(default_nvar, 2)
-get_polygon1_ncon(; n::Integer = default_nvar, kwargs...) = div(default_nvar, 2)
-get_polygon1_nlin(; n::Integer = default_nvar, kwargs...) = div(default_nvar, 2)
+get_polygon1_nvar(; n::Integer = default_nvar, kwargs...) = 2 * div(n, 2)
+get_polygon1_ncon(; n::Integer = default_nvar, kwargs...) = div(n, 2)
+get_polygon1_nlin(; n::Integer = default_nvar, kwargs...) = div(n, 2)
 get_polygon1_nnln(; n::Integer = default_nvar, kwargs...) = 0
 get_polygon1_nequ(; n::Integer = default_nvar, kwargs...) = 1
-get_polygon1_nineq(; n::Integer = default_nvar, kwargs...) = div(default_nvar, 2) - 1
+get_polygon1_nineq(; n::Integer = default_nvar, kwargs...) = div(n, 2) - 1
