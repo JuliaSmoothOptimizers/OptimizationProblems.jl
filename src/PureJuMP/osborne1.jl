0641e34528faafd047f874e14777c27cda0a000f
Philippe M <philippe.maisonneuve@polymtl.ca>
2022-04-19 14:14:08 -0400

    Fix parenthesis

--- src/PureJuMP/osborne1.jl
+++ src/PureJuMP/osborne1.jl
@@ -25,6 +25,6 @@ function osborne1(args...; n::Int = default_nvar, kwargs...)
     @NLobjective(
     nlp,
     Min,
-    sum((y[j] - (x[1]+ x[2] * exp(-j*x[4]) + x[3]*exp(-j*x[5])))^2 for j = 1:m)
+    sum((y[j] - (x[1]+ x[2] * exp(-j*x[4]) + x[3]*exp(-j*x[5])))^2 for j = 1:m))
     return nlp
 end
