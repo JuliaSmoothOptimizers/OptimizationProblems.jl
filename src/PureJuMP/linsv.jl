94b4d599c17be6447ee805bc1b623e20dbd6cba3
tmigot <tangi.migot@gmail.com>
2022-04-20 12:19:18 -0400

    add JuMP model for `linsv` (#159)

---
+++ src/PureJuMP/linsv.jl
@@ -0,0 +1,15 @@
+export linsv
+
+function linsv(args...; n::Int = default_nvar, kwargs...) where {T}
+
+  nlp = Model()
+
+  @variable(nlp, x[1:2], start = 0)
+
+  @objective(nlp, Min, x[1])
+
+  @constraint(nlp, x[1] + x[2] >= 3)
+  @constraint(nlp, x[2] >= 1)
+
+  return nlp
+end
