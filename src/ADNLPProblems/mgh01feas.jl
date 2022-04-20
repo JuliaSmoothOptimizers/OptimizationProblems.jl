c2ccc49c26d3aeec4bdde814b75a31ab0cf6a218
tmigot <tangi.migot@gmail.com>
2022-04-20 12:18:39 -0400

    Add PureJuMP `mgh01feas` (#164)

--- src/ADNLPProblems/mgh01feas.jl
+++ src/ADNLPProblems/mgh01feas.jl
@@ -3,9 +3,9 @@ export mgh01feas
 function mgh01feas(; n::Int = default_nvar, type::Val{T} = Val(Float64), kwargs...) where {T}
   x0 = T[-1.2; 1.0]
   f(x) = zero(T)
-  c(x) = [1 - x[1]; 10 * (x[2] - x[1]^2)]
-  lcon = zeros(T, 2)
-  ucon = zeros(T, 2)
+  c(x) = [- x[1]; 10 * (x[2] - x[1]^2)]
+  lcon = T[-1, 0]
+  ucon = T[-1, 0]
 
-  return ADNLPModels.ADNLPModel(f, x0, c, lcon, ucon, name = "mgh01feas"; kwargs...)
+  return ADNLPModels.ADNLPModel(f, x0, c, lcon, ucon, name = "mgh01feas", lin = [1]; kwargs...)
 end
