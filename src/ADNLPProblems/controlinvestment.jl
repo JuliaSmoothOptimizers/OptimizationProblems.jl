2f184d68c062aefe3d525ad347d5577417011a35
tmigot <tangi.migot@gmail.com>
2022-04-21 17:36:33 -0400

    Add controlinvestment (#155)
    * add `controlinvestment`

---
+++ src/ADNLPProblems/controlinvestment.jl
@@ -0,0 +1,37 @@
+export controlinvestment
+
+function controlinvestment(args...; n::Int = default_nvar, type::Val{T} = Val(Float64), kwargs...) where {T}
+  N = div(n, 2)
+  h = T(1 / N)
+  x0 = 1
+  gamma = 3
+  function f(y)
+    x, u = y[1:N], y[(N + 1):end]
+    return T(0.5) * h * sum((u[k] - 1) * x[k] + (u[k + 1] - 1) * x[k + 1] for k = 1:(N - 1))
+  end
+  function c(y)
+    x, u = y[1:N], y[(N + 1):end]
+    return vcat(
+      x[1],
+      [
+        x[k + 1] - x[k] - T(0.5) * h * gamma * (u[k] * x[k] + u[k + 1] * x[k + 1]) for k = 1:(N - 1)
+      ],
+    )
+  end
+  lvar = vcat(-T(Inf) * ones(T, N), zeros(T, N))
+  uvar = vcat(T(Inf) * ones(T, N), ones(T, N))
+  xi = vcat(ones(T, N), zeros(T, N))
+  return ADNLPModels.ADNLPModel(
+    f,
+    xi,
+    lvar,
+    uvar,
+    c,
+    vcat(one(T), zeros(T, N - 1)),
+    vcat(one(T), zeros(T, N - 1)),
+    name = "controlinvestment",
+    lin = [1],
+    ;
+    kwargs...,
+  )
+end 
---
+++ src/Meta/controlinvestment.jl
@@ -0,0 +1,25 @@
+controlinvestment_meta = Dict(
+  :nvar => 2 * div(default_nvar, 2),
+  :variable_nvar => true,
+  :ncon => div(default_nvar, 2),
+  :variable_ncon => true,
+  :minimize => true,
+  :name => "controlinvestment",
+  :has_equalities_only => true,
+  :has_inequalities_only => false,
+  :has_bounds => true,
+  :has_fixed_variables => false,
+  :objtype => :other,
+  :contype => :general,
+  :best_known_lower_bound => -Inf,
+  :best_known_upper_bound => -0.98,
+  :is_feasible => true,
+  :defined_everywhere => missing,
+  :origin => :unknown,
+)
+get_controlinvestment_nvar(; n::Integer = default_nvar, kwargs...) = 2 * div(default_nvar, 2)
+get_controlinvestment_ncon(; n::Integer = default_nvar, kwargs...) = div(default_nvar, 2)
+get_controlinvestment_nlin(; n::Integer = default_nvar, kwargs...) = 1
+get_controlinvestment_nnln(; n::Integer = default_nvar, kwargs...) = div(default_nvar, 2) - 1
+get_controlinvestment_nequ(; n::Integer = default_nvar, kwargs...) = div(default_nvar, 2)
+get_controlinvestment_nineq(; n::Integer = default_nvar, kwargs...) = 0
