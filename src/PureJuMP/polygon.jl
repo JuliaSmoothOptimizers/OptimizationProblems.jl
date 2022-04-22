5eeaea83fb109b71637872a4ed43ad17d7e6e8b8
tmigot <tmigot@users.noreply.github.com>
2022-04-22 20:44:12 +0000

    :robot: Format .jl files

--- src/PureJuMP/polygon.jl
+++ src/PureJuMP/polygon.jl
@@ -18,7 +18,7 @@ function polygon(args...; n::Int = default_nvar, kwargs...)
   nlp = Model()
   N = div(n, 2)
   @variable(nlp, 0 <= r[1:N] <= 1, start = 1)
-  @variable(nlp, 0 <= θ[i=1:N] <= π, start = i * π / (N - 1) - π / (N - 1))
+  @variable(nlp, 0 <= θ[i = 1:N] <= π, start = i * π / (N - 1) - π / (N - 1))
 
   # impose an order to the angles
   @constraint(nlp, θ[N] == π)
@@ -27,15 +27,11 @@ function polygon(args...; n::Int = default_nvar, kwargs...)
     @constraint(nlp, θ[i + 1] - θ[i] >= 0.0)
   end
   for i = 1:(N - 1)
-    for j=(i + 1):N
+    for j = (i + 1):N
       @NLconstraint(nlp, r[i]^2 + r[j]^2 - 2 * r[i] * r[j] * cos(θ[i] - θ[j]) - 1 <= 0)
     end
   end
 
-  @NLobjective(
-    nlp,
-    Min,
-    -0.5 * sum(r[i] * r[i + 1] * sin(θ[i + 1] - θ[i]) for i = 1:(N - 1))
-  )
+  @NLobjective(nlp, Min, -0.5 * sum(r[i] * r[i + 1] * sin(θ[i + 1] - θ[i]) for i = 1:(N - 1)))
   return nlp
 end
