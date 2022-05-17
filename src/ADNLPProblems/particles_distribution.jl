e56db0b53dfd99e7360ef06cf0bd321f6e0662b4
nathanemac <91251698+nathanemac@users.noreply.github.com>
2022-05-17 14:59:51 -0400

    after D.O adds

--- src/ADNLPProblems/particles_distribution.jl
+++ src/ADNLPProblems/particles_distribution.jl
@@ -1,26 +1,23 @@
 using ADNLPModels, LinearAlgebra, Distances, SolverCore
-function particles_distribution(;n::Int = 20, type::Val{T} = Val(Float64), kwargs...) where {T}
+function particles_distribution(;n::Int = default_nvar, type::Val{T} = Val(Float64), kwargs...) where {T}
 
-    N=n
     # Define the objective function to minimize
     function objective(x)
-        v=reshape(x,3,N)'
+        v=reshape(x,n,3)
         elts=pairwise(Euclidean(),v,dims=1)
-        u_elts=[]
-        for i=2:N-1
-            for j=i+1:N
-                push!(u_elts,1/elts[i,j])
+        s = 0
+        for i=2:n-1
+            for j=i+1:n
+                s += 1/elts[i,j]
             end
         end
-        s=sum(u_elts)
         return(s)
     end
-
 
     # Define the constraints on these points (sum of the square of the coordinates = 1)
     function constraints(x)
         c=[]
-        for k=0:N-1
+        for k=0:n-1
             push!(c, x[3k+1]^2 + x[3k+2]^2 + x[3k+3]^2)
         end
         return c
@@ -29,8 +26,8 @@ function particles_distribution(;n::Int = 20, type::Val{T} = Val(Float64), kwarg
 
     # Solving the problem
 
-    lcon=ucon=(ones(T,N))
-    z0=rand(T,N*3)
+    lcon=ucon=(ones(T,n))
+    x0=rand(T,n*3)
 
-    return ADNLPModel(objective, z0, constraints, lcon, ucon, name="particles distribution on a sphere")
+    return ADNLPModel(objective, x0, constraints, lcon, ucon, name="particles distribution on a sphere")
 end
