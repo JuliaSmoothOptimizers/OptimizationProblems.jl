f1e85a2ad7981c25dbbf8eee7a4be54ca5b700bb
nathanemac <91251698+nathanemac@users.noreply.github.com>
2022-05-23 11:06:27 -0400

    add robotarm

--- src/ADNLPProblems/robotarm.jl
+++ src/ADNLPProblems/robotarm.jl
@@ -1,6 +1,4 @@
-function robotarm(;N::Int = default_nvar,L = 4.5, ϵ = 1e-4, type::Val{T} = Val(Float64), kwargs...) where {T}
-
-    # Minimize the time taken for a robot arm to travel between two points.
+# Minimize the time taken for a robot arm to travel between two points.
 
     #  This is problem 8 in the COPS (Version 3) collection of 
     #   E. Dolan and J. More
@@ -9,7 +7,11 @@ function robotarm(;N::Int = default_nvar,L = 4.5, ϵ = 1e-4, type::Val{T} = Val(
     
     #  classification OOR2-AN-V-V
 
-    n=N+1 # number of timesteps 
+function robotarm(;n::Int = default_nvar,L::T = 4.5, type::Val{T} = Val(Float64), kwargs...) where {T}
+
+    N = max(2, div(n, 9))
+    n = N + 1
+
     # x : vector of variables, of the form : [ρ(t=t1); ρ(t=t2); ... ρ(t=tf), θ(t=t1), ..., then ρ_dot, ..., then ρ_acc, .. ϕ_acc, tf]
     # There are N+1 values of each 9 variables 
     # x = [ρ, θ, ϕ, ρ_dot, θ_dot, ϕ_dot, ρ_acc, θ_acc, ϕ_acc, tf]
@@ -21,18 +23,11 @@ function robotarm(;N::Int = default_nvar,L = 4.5, ϵ = 1e-4, type::Val{T} = Val(
 
     # constraints function 
     function c(x)
-        # position bounds on ρ, θ, ϕ
-        c_ρ = x[1:n]
-        c_θ = x[n+1:2n]
-        c_ϕ = x[2n+1:3n]
 
         # dynamic bounds on ρ_acc, θ_acc, ϕ_acc
         c_ρ_acc = L*x[6n+1:7n]
         c_θ_acc = x[7n+1:8n] .* ((L .- x[1:n]).^3 .+ x[1:n].^3)/3 .* sin.(x[2n+1:3n]).^2
         c_ϕ_acc = x[8n+1:9n] .* ((L .- x[1:n]).^3 .+ x[1:n].^3)/3
-
-        # tf must be positive
-        c_tf = x[end]
 
         # Euler's constraints 
         c_euler1 = x[2:n] - x[1:n-1] - x[3n+1:4n-1]*x[end]/n
@@ -46,35 +41,29 @@ function robotarm(;N::Int = default_nvar,L = 4.5, ϵ = 1e-4, type::Val{T} = Val(
         # constraints on initial and final values 
         c_0_f = [x[1]; x[n]; x[n+1]; x[2n]; x[2n+1]; x[3n]; x[3n+1]; x[4n]; x[4n+1]; x[5n]; x[5n+1]; x[6n]; x[6n+1]; x[7n]; x[7n+1]; x[8n]; x[8n+1]; x[9n]]
 
-        return [c_ρ; c_θ; c_ϕ; c_ρ_acc; c_θ_acc; c_ϕ_acc; c_tf; c_euler; c_0_f]
+        return [c_ρ_acc; c_θ_acc; c_ϕ_acc; c_euler; c_0_f]
     end
 
-    lcon = T[zeros(n);
-            -π*ones(n);
-            zeros(n);
+    lcon = T[-ones(n);
             -ones(n);
             -ones(n);
-            -ones(n);
-            0;
-            zeros(6N) .- ϵ;
+            zeros(6N);
             L; L; 0; 2π/3; π/4; π/4; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0]
 
-    ucon = T[L*ones(n);
-            π*ones(n);
-            π*ones(n);
+    ucon = T[ones(n);
             ones(n);
             ones(n);
-            ones(n);
-            +Inf;
-            zeros(6N) .+ ϵ;
+            zeros(6N);
             L; L; 0; 2π/3; π/4; π/4; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0]
 
     # Building a feasible x0
     tf0=T(1)
-    x0 = [L*ones(T, n); rand(T,n); π/4*ones(T, n); zeros(T, 6n); tf0]
     θ0 = T[2π/3*(t/n)^2 for t in 1:n]
-    x0[n+1:2n] = θ0
-    x0[n+1] = T(0)
+    x0 = [L*ones(T, n); θ0; π*ones(T, n)/4; zeros(T, 6n); tf0]
 
-    return ADNLPModel(f, x0, c, lcon, ucon, name="robotarm")
+    # defining the bounds on the variables
+    lvar = T[zeros(n); -π*ones(n); zeros(n) ; -Inf*ones(6n); 0]
+    uvar = T[L*ones(n); π*ones(n); 2π*ones(n) ; Inf*ones(6n); Inf]
+
+    return ADNLPModel(f, x0, lvar, uvar, c, lcon, ucon, name="robotarm")
 end
--- src/Meta/robotarm.jl
+++ src/Meta/robotarm.jl
@@ -1,13 +1,13 @@
 robotarm_meta = Dict(
-  :nvar => 280,
-  :variable_nvar => false,
-  :ncon => 385,
-  :variable_ncon => false,
+  :nvar => 109,
+  :variable_nvar => true,
+  :ncon => 120,
+  :variable_ncon => true,
   :minimize => true,
   :name => "robotarm",
   :has_equalities_only => false,
   :has_inequalities_only => false,
-  :has_bounds => false,
+  :has_bounds => true,
   :has_fixed_variables => false,
   :objtype => :other,
   :contype => :general,
@@ -17,9 +17,9 @@ robotarm_meta = Dict(
   :defined_everywhere => missing,
   :origin => :unknown,
 )
-get_robotarm_nvar(; n::Integer = default_nvar, kwargs...) = 280
-get_robotarm_ncon(; n::Integer = default_nvar, kwargs...) = 385
+get_robotarm_nvar(; n::Integer = default_nvar, kwargs...) = 1 * n + 5
+get_robotarm_ncon(; n::Integer = default_nvar, kwargs...) = 1 * n + 16
 get_robotarm_nlin(; n::Integer = default_nvar, kwargs...) = 0
-get_robotarm_nnln(; n::Integer = default_nvar, kwargs...) = 385
-get_robotarm_nequ(; n::Integer = default_nvar, kwargs...) = 18
-get_robotarm_nineq(; n::Integer = default_nvar, kwargs...) = 367
+get_robotarm_nnln(; n::Integer = default_nvar, kwargs...) = 1 * n + 16
+get_robotarm_nequ(; n::Integer = default_nvar, kwargs...) = 0 * n + 48
+get_robotarm_nineq(; n::Integer = default_nvar, kwargs...) = 0 * n + 18
