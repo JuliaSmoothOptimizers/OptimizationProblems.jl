a1af2b2db829842c02aed88cd75c55fa6cf9ed73
nathanemac <91251698+nathanemac@users.noreply.github.com>
2022-06-01 20:35:28 +0200

    particles_distribution (#177)
    * add elec
    
    Co-authored-by: tmigot <tangi.migot@gmail.com>

---
+++ src/Meta/elec.jl
@@ -0,0 +1,25 @@
+elec_meta = Dict(
+  :nvar => 99,
+  :variable_nvar => true,
+  :ncon => 33,
+  :variable_ncon => true,
+  :minimize => true,
+  :name => "elec",
+  :has_equalities_only => true,
+  :has_inequalities_only => false,
+  :has_bounds => true,
+  :has_fixed_variables => false,
+  :objtype => :other,
+  :contype => :quadratic,
+  :best_known_lower_bound => -Inf,
+  :best_known_upper_bound => Inf,
+  :is_feasible => missing,
+  :defined_everywhere => missing,
+  :origin => :unknown,
+)
+get_elec_nvar(; n::Integer = default_nvar, kwargs...) = 3 * max(2, div(n,3))
+get_elec_ncon(; n::Integer = default_nvar, kwargs...) = max(2, div(n,3))
+get_elec_nlin(; n::Integer = default_nvar, kwargs...) = 0
+get_elec_nnln(; n::Integer = default_nvar, kwargs...) = max(2, div(n,3))
+get_elec_nequ(; n::Integer = default_nvar, kwargs...) = max(2, div(n,3))
+get_elec_nineq(; n::Integer = default_nvar, kwargs...) = 0
