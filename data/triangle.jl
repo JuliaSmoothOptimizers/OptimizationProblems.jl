ff26a8022e418bcb5977d59e022e975191c51410
tmigot <tmigot@users.noreply.github.com>
2022-06-03 22:51:44 +0000

    :robot: Format .jl files

--- data/triangle.jl
+++ data/triangle.jl
@@ -1,13 +1,16 @@
-xe = [0 0
-     1 0
-     0  1
-     0.64 0.25];
+xe = [
+  0 0
+  1 0
+  0 1
+  0.64 0.25
+];
 
-Tr = [1 2 4
-        4 2 3
-        4 3 1];
+Tr = [
+  1 2 4
+  4 2 3
+  4 3 1
+];
 
 Tr = vec(reshape(Tr, 9, 1));
-xe = vec(reshape(xe, 8,1));
-Constants = [1,2,3];
-
+xe = vec(reshape(xe, 8, 1));
+Constants = [1, 2, 3];
