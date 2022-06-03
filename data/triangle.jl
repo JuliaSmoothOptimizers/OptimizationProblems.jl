5ad3daeba82c2146377741bf2f75490db5fc8a66
MohamedLaghdafHABIBOULLAH <81633807+MohamedLaghdafHABIBOULLAH@users.noreply.github.com>
2022-06-03 14:36:11 -0400

    Update triangle.jl

--- data/triangle.jl
+++ data/triangle.jl
@@ -7,4 +7,6 @@ Tr = [1 2 4
         4 2 3
         4 3 1];
 
+Tr = vec(reshape(Tr, 9, 1));
+xe = vec(reshape(xe, 8,1));
 Constants = [1,2,3];
--- src/ADNLPProblems/triangle.jl
+++ src/ADNLPProblems/triangle.jl
@@ -1,9 +1,19 @@
+# Minimize the sum of the inverse weighted mean ratio of the elements in a fixed–boundary
+# triangular mesh by adjusting the locations of the free vertices.
+
+#  This is problem 18 in the COPS (Version 3) collection of 
+#   E. Dolan and J. More
+#   see "Benchmarking Optimization Software with COPS"
+#   Argonne National Labs Technical Report ANL/MCS-246 (2004)
+
+
 include("../../data/triangle.jl")
 export triangle
 
 function triangle(x0 = xe, TRIS::Vector{Int64} = Tr, Const::Vector{Int64} = Constants; n::Int = default_nvar, type::Val{T} = Val(Float64), kwargs...) where {T}
+  x0 = T.(x0)
   τ = zero(T)
-  n =length(x0)
+  n = length(x0)
   N = Int(div(n,2))
   E = Int(div(length(TRIS),3))
   
@@ -56,4 +66,4 @@ triangle_pacman(;kwargs...) = triangle(xe_pacman, TRIS_pacman, Const_pacman; nam
 
 include("../../data/triangle_turtle.jl")
 export triangle_turtle
-triangle_turtle(; kwargs...) = triangle_turtle(xe_turtle, TRIS_turtle, Const_turtle; name = "triangle_turtle", kwargs...)
+triangle_turtle(; kwargs...) = triangle(xe_turtle, TRIS_turtle, Const_turtle; name = "triangle_turtle", kwargs...)
