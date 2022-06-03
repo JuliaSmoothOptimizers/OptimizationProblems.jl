004fe3144eb9a5d3c042e74f28e30a30ad2cd9a6
tmigot <tmigot@users.noreply.github.com>
2022-06-03 22:51:44 +0000

    :robot: Format .jl files

--- src/ADNLPProblems/camshape.jl
+++ src/ADNLPProblems/camshape.jl
@@ -1,32 +1,31 @@
 export camshape
 
 function camshape(args...; n::Int = default_nvar, type::Val{T} = Val(Float64), kwargs...) where {T}
-    R_min = T(1)
-    R_max = T(2)
-    R_v = T(1)
-    α = T(1.5)
-    θ = T(2*pi/(5*(n+1)))
-    function f(y)
-        return -T(R_v*pi/n) * sum(y[i] for i = 1:n)
-    end
-    function c(y::V) where {V}
-        return vcat(R_max - y[n], y[1] - R_min, [y[i+1] - y[i] for i = 1:(n - 1)], - R_min*y[1] - y[1]*y[2] + 2*R_min*y[2]*cos(θ), - R_min^2 - R_min*y[1] + 2*R_min*y[1]*cos(θ), - y[n-1]*y[n] - y[n]*R_max + 2*y[n-1]*R_max*cos(θ), - 2*R_max*y[n] + 2*y[n]^2*cos(θ), [- y[i-1]*y[i] - y[i]*y[i+1] + 2*y[i-1]*y[i+1]*cos(θ) for i = 2:(n-1)])
-    end
-
-    lvar = T(R_min) * ones(T, n)
-    uvar = T(R_max) * ones(T, n)
-    lcon = vcat(T(-α*θ)*ones(T, n+1), -T(Inf) * ones(T, n+2))
-    ucon = vcat(T(α*θ)*ones(T, n+1), zeros(T, n+2))
-    x0 = T((R_min + R_max)/2) * ones(T, n)
-    return ADNLPModels.ADNLPModel(
-    f,
-    x0,
-    lvar,
-    uvar,
-    c,
-    lcon,
-    ucon,
-    name = "camshape",;
-    kwargs...,
+  R_min = T(1)
+  R_max = T(2)
+  R_v = T(1)
+  α = T(1.5)
+  θ = T(2 * pi / (5 * (n + 1)))
+  function f(y)
+    return -T(R_v * pi / n) * sum(y[i] for i = 1:n)
+  end
+  function c(y::V) where {V}
+    return vcat(
+      R_max - y[n],
+      y[1] - R_min,
+      [y[i + 1] - y[i] for i = 1:(n - 1)],
+      -R_min * y[1] - y[1] * y[2] + 2 * R_min * y[2] * cos(θ),
+      -R_min^2 - R_min * y[1] + 2 * R_min * y[1] * cos(θ),
+      -y[n - 1] * y[n] - y[n] * R_max + 2 * y[n - 1] * R_max * cos(θ),
+      -2 * R_max * y[n] + 2 * y[n]^2 * cos(θ),
+      [-y[i - 1] * y[i] - y[i] * y[i + 1] + 2 * y[i - 1] * y[i + 1] * cos(θ) for i = 2:(n - 1)],
     )
+  end
+
+  lvar = T(R_min) * ones(T, n)
+  uvar = T(R_max) * ones(T, n)
+  lcon = vcat(T(-α * θ) * ones(T, n + 1), -T(Inf) * ones(T, n + 2))
+  ucon = vcat(T(α * θ) * ones(T, n + 1), zeros(T, n + 2))
+  x0 = T((R_min + R_max) / 2) * ones(T, n)
+  return ADNLPModels.ADNLPModel(f, x0, lvar, uvar, c, lcon, ucon, name = "camshape", ; kwargs...)
 end
--- src/PureJuMP/camshape.jl
+++ src/PureJuMP/camshape.jl
@@ -11,30 +11,30 @@
 export camshape
 
 function camshape(args...; n::Int = default_nvar, kwargs...) where {T}
-    R_min = 1
-    R_max = 2
-    R_v = 1
-    α = 1.5
-    θ = 2*pi/(5*(n+1))
+  R_min = 1
+  R_max = 2
+  R_v = 1
+  α = 1.5
+  θ = 2 * pi / (5 * (n + 1))
 
   nlp = Model()
 
-  @variable(nlp, R_min <= x[i = 1:n] <= R_max, start = (R_min + R_max)/2)
-  
-  @NLobjective(nlp, Min, -R_v*pi/n * sum(x[i] for i = 1:n))
+  @variable(nlp, R_min <= x[i = 1:n] <= R_max, start = (R_min + R_max) / 2)
 
-  @NLconstraint(nlp, -α*θ <= R_max - x[n] <= α*θ)
-  @NLconstraint(nlp, -α*θ <= x[1] - R_min <= α*θ)
+  @NLobjective(nlp, Min, -R_v * pi / n * sum(x[i] for i = 1:n))
+
+  @NLconstraint(nlp, -α * θ <= R_max - x[n] <= α * θ)
+  @NLconstraint(nlp, -α * θ <= x[1] - R_min <= α * θ)
   for i = 1:(n - 1)
-    @NLconstraint(nlp, -α*θ <= x[i+1] - x[i] <= α*θ)
+    @NLconstraint(nlp, -α * θ <= x[i + 1] - x[i] <= α * θ)
   end
-  @NLconstraint(nlp, - R_min*x[1] - x[1]*x[2] + 2*R_min*x[2]*cos(θ) <= 0 )
-  @NLconstraint(nlp, - R_min^2 - R_min*x[1] + 2*R_min*x[1]*cos(θ) <= 0 )
-  @NLconstraint(nlp, - x[n-1]*x[n] - x[n]*R_max + 2*x[n-1]*R_max*cos(θ) <= 0 )
-  @NLconstraint(nlp, - 2*R_max*x[n] + 2*x[n]^2*cos(θ) <= 0 )
-  for i = 2:(n-1)
-    @NLconstraint(nlp, - x[i-1]*x[i] - x[i]*x[i+1] + 2*x[i-1]*x[i+1]*cos(θ) <= 0 )
+  @NLconstraint(nlp, -R_min * x[1] - x[1] * x[2] + 2 * R_min * x[2] * cos(θ) <= 0)
+  @NLconstraint(nlp, -R_min^2 - R_min * x[1] + 2 * R_min * x[1] * cos(θ) <= 0)
+  @NLconstraint(nlp, -x[n - 1] * x[n] - x[n] * R_max + 2 * x[n - 1] * R_max * cos(θ) <= 0)
+  @NLconstraint(nlp, -2 * R_max * x[n] + 2 * x[n]^2 * cos(θ) <= 0)
+  for i = 2:(n - 1)
+    @NLconstraint(nlp, -x[i - 1] * x[i] - x[i] * x[i + 1] + 2 * x[i - 1] * x[i + 1] * cos(θ) <= 0)
   end
-  
+
   return nlp
 end
