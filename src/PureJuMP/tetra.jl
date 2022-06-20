3da1e4ba771718a0b4fcb271cee93cccb40328ac
tmigot <tmigot@users.noreply.github.com>
2022-06-20 20:17:55 +0000

    :robot: Format .jl files

--- src/PureJuMP/tetra.jl
+++ src/PureJuMP/tetra.jl
@@ -17,18 +17,18 @@ function tetra(
 )
   τ = 0.0
   n = length(x0)
-  N = round(Int,n/3)
-  E = round(Int,length(TETS)/4)
+  N = round(Int, n / 3)
+  E = round(Int, length(TETS) / 4)
 
   lvar = -Inf * ones(n)
   lvar[Const] = x0[Const]
-  lvar[Const.+N] = x0[Const.+N]
-  lvar[Const.+ 2*N] = x0[Const.+ 2*N]
-    
+  lvar[Const .+ N] = x0[Const .+ N]
+  lvar[Const .+ 2 * N] = x0[Const .+ 2 * N]
+
   uvar = Inf * ones(n)
   uvar[Const] = x0[Const]
-  uvar[Const.+N] = x0[Const.+N]
-  uvar[Const.+ 2*N] = x0[Const.+ 2*N]
+  uvar[Const .+ N] = x0[Const .+ N]
+  uvar[Const .+ 2 * N] = x0[Const .+ 2 * N]
 
   nlp = Model()
 
@@ -37,13 +37,43 @@ function tetra(
   @NLobjective(
     nlp,
     Min,
-    sum((sum((1*x[TETS[e+E]+N*i] - x[TETS[e]+N*i])^2 + (2*x[TETS[e+2*E]+N*i] - x[TETS[e+E]+N*i] - x[TETS[e]+N*i])^2 / 3 + (3*x[TETS[e+3*E]+N*i] - x[TETS[e+2*E]+N*i] - x[TETS[e+E]+N*i] - x[TETS[e]+N*i])^2 / 6 for i=0:2))/(3*(sum((x[TETS[e+E]+N*i] - x[TETS[e]+N*i])*((x[TETS[e+2*E]+N*mod(i+1,3)]-x[TETS[e]+N*mod(i+1,3)])*(x[TETS[e+3*E]+N*mod(i-1,3)]-x[TETS[e]+N*mod(i-1,3)])- (x[TETS[e+2*E]+N*mod(i-1,3)]-x[TETS[e]+N*mod(i-1,3)])*(x[TETS[e+3*E]+N*mod(i+1,3)]-x[TETS[e]+N*mod(i+1,3)])) * sqrt(2) for i=0:2))^(2/3)) for e=1:E)
+    sum(
+      (sum(
+        (1 * x[TETS[e + E] + N * i] - x[TETS[e] + N * i])^2 +
+        (2 * x[TETS[e + 2 * E] + N * i] - x[TETS[e + E] + N * i] - x[TETS[e] + N * i])^2 / 3 +
+        (
+          3 * x[TETS[e + 3 * E] + N * i] - x[TETS[e + 2 * E] + N * i] - x[TETS[e + E] + N * i] -
+          x[TETS[e] + N * i]
+        )^2 / 6 for i = 0:2
+      )) / (
+        3 *
+        (sum(
+          (x[TETS[e + E] + N * i] - x[TETS[e] + N * i]) *
+          (
+            (x[TETS[e + 2 * E] + N * mod(i + 1, 3)] - x[TETS[e] + N * mod(i + 1, 3)]) *
+            (x[TETS[e + 3 * E] + N * mod(i - 1, 3)] - x[TETS[e] + N * mod(i - 1, 3)]) -
+            (x[TETS[e + 2 * E] + N * mod(i - 1, 3)] - x[TETS[e] + N * mod(i - 1, 3)]) *
+            (x[TETS[e + 3 * E] + N * mod(i + 1, 3)] - x[TETS[e] + N * mod(i + 1, 3)])
+          ) *
+          sqrt(2) for i = 0:2
+        ))^(2 / 3)
+      ) for e = 1:E
+    )
   )
 
   for e = 1:E
     @NLconstraint(
       nlp,
-      sum((x[TETS[e+E]+N*i] - x[TETS[e]+N*i])*((x[TETS[e+2*E]+N*mod(i+1,3)]-x[TETS[e]+N*mod(i+1,3)])*(x[TETS[e+3*E]+N*mod(i-1,3)]-x[TETS[e]+N*mod(i-1,3)])- (x[TETS[e+2*E]+N*mod(i-1,3)]-x[TETS[e]+N*mod(i-1,3)])*(x[TETS[e+3*E]+N*mod(i+1,3)]-x[TETS[e]+N*mod(i+1,3)])) * sqrt(2) for i=0:2) >= τ
+      sum(
+        (x[TETS[e + E] + N * i] - x[TETS[e] + N * i]) *
+        (
+          (x[TETS[e + 2 * E] + N * mod(i + 1, 3)] - x[TETS[e] + N * mod(i + 1, 3)]) *
+          (x[TETS[e + 3 * E] + N * mod(i - 1, 3)] - x[TETS[e] + N * mod(i - 1, 3)]) -
+          (x[TETS[e + 2 * E] + N * mod(i - 1, 3)] - x[TETS[e] + N * mod(i - 1, 3)]) *
+          (x[TETS[e + 3 * E] + N * mod(i + 1, 3)] - x[TETS[e] + N * mod(i + 1, 3)])
+        ) *
+        sqrt(2) for i = 0:2
+      ) >= τ
     )
   end
 
@@ -52,24 +82,24 @@ end
 
 include("../../data/tetra_duct12.jl")
 export tetra_duct12
-tetra_duct12(;kwargs...) = tetra(xe_duct12, TETS_duct12, Const_duct12; kwargs...)
+tetra_duct12(; kwargs...) = tetra(xe_duct12, TETS_duct12, Const_duct12; kwargs...)
 
 include("../../data/tetra_duct15.jl")
 export tetra_duct15
-tetra_duct15(;kwargs...) = tetra(xe_duct15, TETS_duct15, Const_duct15; kwargs...)
+tetra_duct15(; kwargs...) = tetra(xe_duct15, TETS_duct15, Const_duct15; kwargs...)
 
 include("../../data/tetra_duct20.jl")
 export tetra_duct20
-tetra_duct20(;kwargs...) = tetra(xe_duct20, TETS_duct20, Const_duct20; kwargs...)
+tetra_duct20(; kwargs...) = tetra(xe_duct20, TETS_duct20, Const_duct20; kwargs...)
 
 include("../../data/tetra_hook.jl")
 export tetra_hook
-tetra_hook(;kwargs...) = tetra(xe_hook, TETS_hook, Const_hook; kwargs...)
+tetra_hook(; kwargs...) = tetra(xe_hook, TETS_hook, Const_hook; kwargs...)
 
 include("../../data/tetra_foam5.jl")
 export tetra_foam5
-tetra_foam5(;kwargs...) = tetra(xe_foam5, TETS_foam5, Const_foam5; kwargs...)
+tetra_foam5(; kwargs...) = tetra(xe_foam5, TETS_foam5, Const_foam5; kwargs...)
 
 include("../../data/tetra_gear.jl")
 export tetra_gear
-tetra_gear(;kwargs...) = tetra(xe_gear, TETS_gear, Const_gear; kwargs...)
+tetra_gear(; kwargs...) = tetra(xe_gear, TETS_gear, Const_gear; kwargs...)
