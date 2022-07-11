75c8bdfdf24457939b47e45076b6faa04f8bdeb6
tmigot <tangi.migot@gmail.com>
2022-07-11 12:44:42 +0200

    add `ADNLSModel` constructor in `arglina`

--- src/ADNLPProblems/arglina.jl
+++ src/ADNLPProblems/arglina.jl
@@ -1,6 +1,11 @@
 export arglina
 
-function arglina(; n::Int = default_nvar, type::Val{T} = Val(Float64), kwargs...) where {T}
+function arglina(;use_nls = false, kwargs...)
+  model = use_nls ? :nls : nlp
+  return arglina(Val(model); kwargs...)
+end
+
+function arglina(::Val{:nlp}; n::Int = default_nvar, type::Val{T} = Val(Float64), kwargs...) where {T}
   function f(x; n = length(x))
     m = 2 * n
     sj = sum(x[j] for j = 1:n)
@@ -8,4 +13,18 @@ function arglina(; n::Int = default_nvar, type::Val{T} = Val(Float64), kwargs...
   end
   x0 = ones(T, n)
   return ADNLPModels.ADNLPModel(f, x0, name = "arglina"; kwargs...)
+end
+
+function arglina(::Val{:nls}; n::Int = default_nvar, type::Val{T} = Val(Float64), kwargs...) where {T}
+  function F!(r, x)
+    m = 2 * n
+    sj = sum(x[j] for j = 1:n)
+    for i=1:n
+      r[i] = x[i] - 2 // m * sj - 1
+      r[i + n] = - 2 // m * sj - 1
+    end
+    return r
+  end
+  x0 = ones(T, n)
+  return ADNLPModels.ADNLSModel!(F!, x0, 2 * n, name = "arglina-nls"; kwargs...)
 end
