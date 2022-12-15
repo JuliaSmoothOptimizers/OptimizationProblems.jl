d82099ade340e3f4c6e8c156c8e56b20e14b23fa
tmigot <tangi.migot@gmail.com>
2022-12-15 11:28:54 -0500

    add `SpecialFunctions`

--- src/ADNLPProblems/ADNLPProblems.jl
+++ src/ADNLPProblems/ADNLPProblems.jl
@@ -8,7 +8,7 @@ reshape_array(a, dims) = invoke(Base._reshape, Tuple{AbstractArray, typeof(dims)
 
 @init begin
   @require ADNLPModels = "54578032-b7ea-4c30-94aa-7cbd1cce6c9a" begin
-    using JLD2, LinearAlgebra, SparseArrays
+    using JLD2, LinearAlgebra, SparseArrays, SpecialFunctions
 
     path = dirname(@__FILE__)
     data_path = joinpath(@__DIR__, "..", "..", "data")
