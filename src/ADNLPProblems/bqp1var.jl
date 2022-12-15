d21e6c40bce411c7f3b2f2bdafc9d709da9a921b
tmigot <tangi.migot@gmail.com>
2022-12-15 09:29:14 -0500

    Add `bqp1var`

---
+++ src/ADNLPProblems/bqp1var.jl
@@ -0,0 +1,24 @@
+export bqp1var
+
+function bqp1var(; n::Int = default_nvar, type::Val{T} = Val(Float64), kwargs...) where {T}
+
+
+  function f(x)
+    return x[1]+x[1]^2
+  end
+  x0 = T[
+    0.25,
+  ]
+  function c(x)
+    return x
+  end
+  lcon = T[
+    0.0,
+  ]
+  ucon = T[
+    0.5,
+  ]
+
+  return ADNLPModels.ADNLPModel(f, x0, c, lcon, ucon, name = "bqp1var", minimize = true; kwargs...)
+end
+
---
+++ src/PureJuMP/bqp1var.jl
@@ -0,0 +1,34 @@
+#
+# Converted in Julia from https://github.com/mpf/Optimization-Test-Problems
+#
+# AMPL Model by Hande Y. Benson
+#
+# Copyright (C) 2001 Princeton University
+# All Rights Reserved
+#
+# Permission to use, copy, modify, and distribute this software and
+# its documentation for any purpose and without fee is hereby
+# granted, provided that the above copyright notice appear in all
+# copies and that the copyright notice and this
+# permission notice appear in all supporting documentation.
+#   Source: a one variable box-constrained quadratic
+#   SIF input: Nick Gould, March 1992
+#   classification QBR2-AN-1-0
+export bqp1var
+
+function bqp1var(; n::Int = default_nvar, kwargs...)
+  nlp = Model()
+
+
+  @variable(nlp, -Inf <= x1 <= Inf, start = 0.25)
+
+  @NLobjective(nlp, Min, 
+    x1+x1^2
+  )
+  @NLconstraint(nlp, cons1,
+0.0 <= x1 <= 0.5
+  )
+
+  return nlp
+end
+
