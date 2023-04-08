c757e3a70c598009a2d21b9cb0e11cd443266587
tmigot <tangi.migot@gmail.com>
2023-04-08 02:30:50 -0400

    add `argtrig`

---
+++ src/ADNLPProblems/argtrig.jl
@@ -0,0 +1,13 @@
+export argtrig
+
+function argtrig(
+  ;n::Int = default_nvar,
+  type::Val{T} = Val(Float64),
+  kwargs...,
+) where {T}
+  function f(x; n = length(x))
+    return n - sum(cos(x[j]) + j * (1 - cos(x[j])) - sin(x[j]) for j=1:n)
+  end
+  x0 = 1/n * ones(T, n)
+  return ADNLPModels.ADNLPModel(f, x0, name = "argtrig"; kwargs...)
+end
