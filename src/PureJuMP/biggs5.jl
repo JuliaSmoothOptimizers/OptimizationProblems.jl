83032aa4afa14a7b2a6b4fdfc1f894b5265aa098
tmigot <tangi.migot@gmail.com>
2023-04-08 02:31:19 -0400

    add `biggs5`

---
+++ src/PureJuMP/biggs5.jl
@@ -0,0 +1,35 @@
+#   Biggs problem in 5 variables.
+#   This function is a nonlinear least squares with 13 groups.  It is a
+#   variation on the biggs6 problem where the 6-th variable is fixed to 3.
+#
+#   Source: Inspited by problem 18 in
+#      J.J. More', B.S. Garbow and K.E. Hillstrom,
+#      "Testing Unconstrained Optimization Software",
+#      ACM Transactions on Mathematical Software, vol. 7(1), pp. 17-41, 1981.
+#   Also problem 74 in
+#      A.R. Buckley,
+#      "Test functions for unconstrained minimization",
+#      TR 1989CS-3, Mathematics, statistics and computing centre,
+#      Dalhousie University, Halifax (CDN), 1989.
+#
+# classification SXR2-AN-6-0
+export biggs5
+
+function biggs5(args...; n::Int = default_nvar, m::Int = 13, kwargs...)
+  m = max(6, m)
+  z = [i / 10 for i = 1:m]
+  y = [exp(-zi) - 5 * exp(-10 * zi) + 3 * exp(-4 * zi) for zi in z]
+  nlp = Model()
+  x0 = [1.0; 2.0; 1.0; 1.0; 1.0; 1.0]
+  @variable(nlp, x[i = 1:6], start = x0[i])
+  fix(x[6], 3)
+  @NLobjective(
+    nlp,
+    Min,
+    sum(
+      x[3] * exp(-x[1] * z[i]) - x[4] * exp(-x[2] * z[i]) + x[6] * exp(-x[5] * z[i]) - y[i] for
+      i = 1:m
+    )
+  )
+  return nlp
+end
