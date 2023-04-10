258880ba8f34dd004ed9b021e871051016d08de2
tmigot <tangi.migot@gmail.com>
2023-04-10 11:14:42 -0400

    Add `penalty1`

---
+++ src/ADNLPProblems/penalty1.jl
@@ -0,0 +1,37 @@
+export penalty1
+
+function penalty1(; use_nls::Bool = false, kwargs...)
+  model = use_nls ? :nls : :nlp
+  return penalty1(Val(model); kwargs...)
+end
+
+function penalty1(
+  ::Val{:nlp};
+  n::Int = default_nvar,
+  type::Val{T} = Val(Float64),
+  kwargs...,
+) where {T}
+  function f(x; n = length(x), a = eltype(x)(sqrt(1e-5)))
+    return 1 // 2 * sum((a * (x[i] - 1))^2 for i = 1:n) +
+           1 // 2 * (sum(x[j]^2 for j=1:n) - 1 // 4)^2
+  end
+  x0 = T[j for j=1:n]
+  return ADNLPModels.ADNLPModel(f, x0, name = "penalty1"; kwargs...)
+end
+
+function penalty1(
+  ::Val{:nls};
+  n::Int = default_nvar,
+  type::Val{T} = Val(Float64),
+  kwargs...,
+) where {T}
+  function F!(r, x; n = length(x), a = eltype(x)(sqrt(1e-5)))
+    for i = 1:n
+      r[i] = a * (x[i] - 1)
+    end
+    r[n + 1] = sum(x[j]^2 for j=1:n) - 1 // 4
+    return r
+  end
+  x0 = ones(T, n)
+  return ADNLPModels.ADNLSModel!(F!, x0, n + 1, name = "penalty1-nls"; kwargs...)
+end
---
+++ src/PureJuMP/penalty1.jl
@@ -0,0 +1,33 @@
+#   A penalty function arising from
+#   min{ sum_1^n(x_i-1)^2 subject to sum_1^n x_i^2 = 1/4}.
+#   At the solution, the hessian has n-1 eigenvalues of order 1e-5 and
+#   one of order 1.
+#
+#   Source: problem 23 in
+#      J.J. More, B.S. Garbow and K.E. Hillstrom,
+#      "Testing Unconstrained Optimization Software",
+#      ACM Transactions on Mathematical Software, vol. 7(1), pp. 17-41, 1981.
+#
+#   See also Buckley #181 (p. 79)
+#
+#   classification SUR2-AN-V-0
+
+export penalty1
+
+"Linear function with `n` parameters and `m` observations  - full rank"
+function penalty1(args...; n::Int = default_nvar, kwargs...)
+  a = sqrt(1e-5)
+
+  nlp = Model()
+
+  @variable(nlp, x[j = 1:n])
+  set_start_value.(x, [j for j=1:n])
+
+  @NLobjective(
+    nlp,
+    Min,
+    1 / 2 * sum((a * (x[i] - 1))^2 for i = 1:n) + 1 / 2 * (sum(x[j]^2 for j=1:n) - 1 / 4)^2
+  )
+
+  return nlp
+end
