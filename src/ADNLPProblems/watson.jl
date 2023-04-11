bd4baa0eed78de2662759e8bc4f838f5445b1cbe
tmigot <tangi.migot@gmail.com>
2023-04-11 14:16:13 -0400

    fix typo `watson`

--- src/ADNLPProblems/watson.jl
+++ src/ADNLPProblems/watson.jl
@@ -10,8 +10,8 @@ function watson(::Val{:nlp}; n::Int = default_nvar, type::Val{T} = Val(Float64),
   function f(x; n = n)
     Ti = eltype(x)
     return 1 // 2 * sum((
-      sum((j - 1) * x[j] * (Ti(i) / 29)^(j-2) for j=2:n) - sum(x[j] * (Ti(i) / 29)^(j-2) for j=1:n)^2 - 1
-    )^2 for i=1:29) + 1 // 2 * (sum((j - 1) * x[j] * x[1]^(j-2) for j=2:n) - sum(x[j] * x[1]^(j-2) for j=1:n)^2 - 1)^2 + 1 // 2 * (sum((j - 1) * x[j] * (x[2] - x[1]^2 - 1)^(j-2) for j=2:n) - sum(x[j] * (x[2] - x[1]^2 - 1)^(j-2) for j=1:n)^2 - 1)
+      sum((j - 1) * x[j] * (Ti(i) / 29)^(j-2) for j=2:n) - sum(x[j] * (Ti(i) / 29)^(j-1) for j=1:n)^2 - 1
+    )^2 for i=1:29) + 1 // 2 * (sum((j - 1) * x[j] * x[1]^(j-2) for j=2:n) - sum(x[j] * x[1]^(j-1) for j=1:n)^2 - 1)^2 + 1 // 2 * (sum((j - 1) * x[j] * (x[2] - x[1]^2 - 1)^(j-2) for j=2:n) - sum(x[j] * (x[2] - x[1]^2 - 1)^(j-1) for j=1:n)^2 - 1)
   end
   x0 = zeros(T, n)
   return ADNLPModels.ADNLPModel(f, x0, name = "watson"; kwargs...)
@@ -22,10 +22,10 @@ function watson(::Val{:nls}; n::Int = default_nvar, type::Val{T} = Val(Float64),
   function F!(r, x; n = n)
     Ti = eltype(x)
     for i=1:29
-      r[i] = sum((j - 1) * x[j] * (Ti(i) / 29)^(j-2) for j=2:n) - sum(x[j] * (Ti(i) / 29)^(j-2) for j=1:n)^2 - 1
+      r[i] = sum((j - 1) * x[j] * (Ti(i) / 29)^(j-2) for j=2:n) - sum(x[j] * (Ti(i) / 29)^(j-1) for j=1:n)^2 - 1
     end
-    r[30] = sum((j - 1) * x[j] * x[1]^(j-2) for j=2:n) - sum(x[j] * x[1]^(j-2) for j=1:n)^2 - 1
-    r[31] = sum((j - 1) * x[j] * (x[2] - x[1]^2 - 1)^(j-2) for j=2:n) - sum(x[j] * (x[2] - x[1]^2 - 1)^(j-2) for j=1:n)^2 - 1
+    r[30] = sum((j - 1) * x[j] * x[1]^(j-2) for j=2:n) - sum(x[j] * x[1]^(j-1) for j=1:n)^2 - 1
+    r[31] = sum((j - 1) * x[j] * (x[2] - x[1]^2 - 1)^(j-2) for j=2:n) - sum(x[j] * (x[2] - x[1]^2 - 1)^(j-1) for j=1:n)^2 - 1
     return r
   end
   x0 = zeros(T, n)
--- src/PureJuMP/watson.jl
+++ src/PureJuMP/watson.jl
@@ -16,8 +16,7 @@
 
 export watson
 
-"Linear function with `n` parameters and `m` observations  - full rank"
-function watson(args...; n::Int = default_nvar, m::Int = 100, kwargs...)
+function watson(args...; n::Int = default_nvar, kwargs...)
   n = min(max(n, 2), 31)
   m = 31
 
@@ -29,8 +28,8 @@ function watson(args...; n::Int = default_nvar, m::Int = 100, kwargs...)
     nlp,
     Min,
     0.5 * sum((
-      sum((j - 1) * x[j] * (i / 29)^(j-2) for j=2:n) - sum(x[j] * (i / 29)^(j-2) for j=1:n)^2 - 1
-    )^2 for i=1:29) + 0.5 * (sum((j - 1) * x[j] * x[1]^(j-2) for j=2:n) - sum(x[j] * x[1]^(j-2) for j=1:n)^2 - 1)^2 + 0.5 * (sum((j - 1) * x[j] * (x[2] - x[1]^2 - 1)^(j-2) for j=2:n) - sum(x[j] * (x[2] - x[1]^2 - 1)^(j-2) for j=1:n)^2 - 1)
+      sum((j - 1) * x[j] * (i / 29)^(j-2) for j=2:n) - sum(x[j] * (i / 29)^(j-1) for j=1:n)^2 - 1
+    )^2 for i=1:29) + 0.5 * (sum((j - 1) * x[j] * x[1]^(j-2) for j=2:n) - sum(x[j] * x[1]^(j-1) for j=1:n)^2 - 1)^2 + 0.5 * (sum((j - 1) * x[j] * (x[2] - x[1]^2 - 1)^(j-2) for j=2:n) - sum(x[j] * (x[2] - x[1]^2 - 1)^(j-1) for j=1:n)^2 - 1)
   )
 
   return nlp
