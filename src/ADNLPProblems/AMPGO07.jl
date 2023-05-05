5d5def9e4576459b655c058ad78c50d5cd614f86
tmigot <tangi.migot@gmail.com>
2023-05-05 17:18:13 -0400

    Fix for ifelse problems

--- src/ADNLPProblems/AMPGO07.jl
+++ src/ADNLPProblems/AMPGO07.jl
@@ -2,8 +2,7 @@ export AMPGO07
 
 function AMPGO07(; n::Int = default_nvar, type::Val{T} = Val(Float64), kwargs...) where {T}
   function f(x)
-    return x[1] <= 0 ? convert(eltype(x), Inf) :
-           sin(x[1]) + sin(10 // 3 * x[1]) + log(abs(x[1])) - 84 // 100 * x[1] + 3
+    return (x[1] <= 0) * convert(eltype(x), Inf) + sin(x[1]) + sin(10 // 3 * x[1]) + log(abs(x[1])) - 84 // 100 * x[1] + 3
   end
   x0 = T(2.7) * ones(T, 1)
   return ADNLPModels.ADNLPModel(f, x0, name = "AMPGO07"; kwargs...)
