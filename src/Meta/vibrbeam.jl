278ede59de8f9114f72af5f8979ff9b0f1e73efc
tmigot <tangi.migot@gmail.com>
2023-05-05 21:00:11 -0400

    Add `VIBRBEAM` (#270)

---
+++ src/Meta/vibrbeam.jl
@@ -0,0 +1,26 @@
+vibrbeam_meta = Dict(
+  :nvar => 8,
+  :variable_nvar => false,
+  :ncon => 0,
+  :variable_ncon => false,
+  :minimize => true,
+  :name => "vibrbeam",
+  :has_equalities_only => false,
+  :has_inequalities_only => false,
+  :has_bounds => false,
+  :has_fixed_variables => false,
+  :objtype => :least_squares,
+  :contype => :unconstrained,
+  :best_known_lower_bound => -Inf,
+  :best_known_upper_bound => 8231.27506726856,
+  :is_feasible => true,
+  :defined_everywhere => missing,
+  :origin => :unknown,
+)
+get_vibrbeam_nvar(; n::Integer = default_nvar, kwargs...) = 8
+get_vibrbeam_ncon(; n::Integer = default_nvar, kwargs...) = 0
+get_vibrbeam_nlin(; n::Integer = default_nvar, kwargs...) = 0
+get_vibrbeam_nnln(; n::Integer = default_nvar, kwargs...) = 0
+get_vibrbeam_nequ(; n::Integer = default_nvar, kwargs...) = 0
+get_vibrbeam_nineq(; n::Integer = default_nvar, kwargs...) = 0
+get_vibrbeam_nls_nequ(; n::Integer = default_nvar, kwargs...) = 30
