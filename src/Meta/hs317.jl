0f716423c8422935e2ce7c46a048e69b8bf58b88
MaxenceGollier <134112149+MaxenceGollier@users.noreply.github.com>
2023-05-22 21:17:42 +0000

    new file:   src/ADNLPProblems/hs316.jl 	new file:   src/ADNLPProblems/hs317.jl 	new file:   src/ADNLPProblems/hs318.jl 	new file:   src/ADNLPProblems/hs319.jl 	new file:   src/ADNLPProblems/hs320.jl 	new file:   src/Meta/hs316.jl 	new file:   src/Meta/hs317.jl 	new file:   src/Meta/hs318.jl 	new file:   src/Meta/hs319.jl 	new file:   src/Meta/hs320.jl 	new file:   src/PureJuMP/hs316.jl 	new file:   src/PureJuMP/hs317.jl 	new file:   src/PureJuMP/hs318.jl 	new file:   src/PureJuMP/hs319.jl 	new file:   src/PureJuMP/hs320.jl

---
+++ src/Meta/hs317.jl
@@ -0,0 +1,25 @@
+hs317_meta = Dict(
+  :nvar => 2,
+  :variable_nvar => false,
+  :ncon => 1,
+  :variable_ncon => false,
+  :minimize => true,
+  :name => "hs317",
+  :has_equalities_only => true,
+  :has_inequalities_only => false,
+  :has_bounds => false,
+  :has_fixed_variables => false,
+  :objtype => :quadratic,
+  :contype => :quadratic,
+  :best_known_lower_bound => -Inf,
+  :best_known_upper_bound => Inf,
+  :is_feasible => true,
+  :defined_everywhere => missing,
+  :origin => :academic,
+)
+get_hs317_nvar(; n::Integer = default_nvar, kwargs...) = 2
+get_hs317_ncon(; n::Integer = default_nvar, kwargs...) = 1
+get_hs317_nlin(; n::Integer = default_nvar, kwargs...) = 0
+get_hs317_nnln(; n::Integer = default_nvar, kwargs...) = 1
+get_hs317_nequ(; n::Integer = default_nvar, kwargs...) = 1
+get_hs317_nineq(; n::Integer = default_nvar, kwargs...) = 0
