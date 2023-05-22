fb4595e16eed6576dcbc133acfd3ff6274f15252
MaxenceGollier <134112149+MaxenceGollier@users.noreply.github.com>
2023-05-22 21:24:26 +0000

    new file:   src/ADNLPProblems/hs321.jl 	new file:   src/ADNLPProblems/hs322.jl 	new file:   src/Meta/hs321.jl 	new file:   src/Meta/hs322.jl 	new file:   src/PureJuMP/hs321.jl 	new file:   src/PureJuMP/hs322.jl

---
+++ src/ADNLPProblems/hs321.jl
@@ -0,0 +1,14 @@
+export hs321
+
+function hs321(; n::Int = default_nvar, type::Val{T} = Val(Float64), kwargs...) where {T}
+  function f(x;n = length(x))
+    return (x[1]-20)^2 + (x[2]+20)^2
+  end
+  function c!(cx,x)
+    cx[1] = x[1]^2/100+x[2]^2/1-1
+  end
+  x0 = zeros(T,2)
+  lcon = zero(T)
+  ucon = zero(T)
+  return ADNLPModels.ADNLPModel(f, x0,c!,lcon,ucon name = "hs321"; kwargs...)
+end
