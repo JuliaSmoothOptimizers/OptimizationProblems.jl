6cadec93cd077955d58b81b451e6a61c8846c18e
MaxenceGollier <134112149+MaxenceGollier@users.noreply.github.com>
2023-06-09 09:28:15 -0400

    Update src/ADNLPProblems/hs321.jl
    Co-authored-by: tmigot <tangi.migot@gmail.com>

--- src/ADNLPProblems/hs321.jl
+++ src/ADNLPProblems/hs321.jl
@@ -5,7 +5,7 @@ function hs321(; n::Int = default_nvar, type::Val{T} = Val(Float64), kwargs...) 
     return (x[1]-20)^2 + (x[2]+20)^2
   end
   function c!(cx,x)
-    cx[1] = x[1]^2/100+x[2]^2/1-1
+    cx[1] = x[1]^2/100+x[2]^2-1
     return cx
   end
   x0 = zeros(T,2)
