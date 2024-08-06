2ae06f47edc5bb21d2eff087b608989311e4b856
Alexis Montoison <35051714+amontoison@users.noreply.github.com>
2024-08-06 16:04:59 -0400

    Update make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -8,6 +8,7 @@ makedocs(
   format = Documenter.HTML(
     assets = ["assets/style.css"],
     prettyurls = get(ENV, "CI", nothing) == "true",
+    size_threshold_ignore = ["reference.md"],
   ),
   sitename = "OptimizationProblems.jl",
   pages = [
