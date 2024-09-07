65f07be58edb3645eb98948ef593ac64f371bea7
tmigot <tmigot@users.noreply.github.com>
2024-09-07 15:17:08 +0000

    :robot: Format .jl files

--- src/PureJuMP/hs201.jl
+++ src/PureJuMP/hs201.jl
@@ -7,19 +7,19 @@
 #   Lectures Notes in Economics and Mathematical Systems 282,
 #   Springer Verlag, Heidelberg, 1987.
 #
-export hs201 
+export hs201
 "HS201 model"
 function hs201(args...; n::Int = default_nvar, kwargs...)
-    model = Model()
+  model = Model()
 
-    #Déclaration des variables
-    @variable(model, x1)
-    set_start_value(x1, 8)
-    @variable(model, x2)
-    set_start_value(x2, 9)
+  #Déclaration des variables
+  @variable(model, x1)
+  set_start_value(x1, 8)
+  @variable(model, x2)
+  set_start_value(x2, 9)
 
-    #Définition de la fonction objectif
-    @objective(model, Min, 4*(x1-5)^2+(x2-6)^2)
+  #Définition de la fonction objectif
+  @objective(model, Min, 4 * (x1 - 5)^2 + (x2 - 6)^2)
 
-    return model
+  return model
 end
