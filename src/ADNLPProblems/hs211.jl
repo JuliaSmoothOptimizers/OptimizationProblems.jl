c724446240c33d8c6fec8d87398efe987e9bc292
Tangi Migot <tangi.migot@gmail.com>
2024-09-07 17:15:51 +0200

    Add HS211 (#346)
    * Add HS211

---
+++ src/ADNLPProblems/hs211.jl
@@ -0,0 +1,8 @@
+export hs211
+
+function hs211(; n::Int = default_nvar, type::Type{T} = Float64, kwargs...) where {T}
+  f(x) = 100*(x[2] - x[1]^3)^2 + (1 - x[1])^2
+  x0 = T[-1.2, 1.]
+  nlp = ADNLPModels.ADNLPModel(f, x0, name = "hs211"; kwargs...)
+  return nlp
+end
