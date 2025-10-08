7c5f40683c8f437f5c72259432fcf781888a5e69
Arnav Kapoor <kapoorarnav43@gmail.com>
2025-10-09 01:02:37 +0530

    Add convenience wrapper auglag() to ADNLPProblems.auglag

--- src/ADNLPProblems/auglag.jl
+++ src/ADNLPProblems/auglag.jl
@@ -1,5 +1,10 @@
 export auglag
 
+function auglag(; use_nls::Bool = false, kwargs...)
+  model = use_nls ? :nls : :nlp
+  return auglag(Val(model); kwargs...)
+end
+
 function auglag(::Val{:nlp}; n::Int = default_nvar, type::Type{T} = Float64, kwargs...) where {T}
   function f(x; n = length(x))
     # Placeholder augmented Lagrangian style objective
