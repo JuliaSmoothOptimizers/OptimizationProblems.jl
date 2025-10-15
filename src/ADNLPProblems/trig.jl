56dc762078bbcee14ab2ea8b0985210d86bd5eb6
Arnav Kapoor <kapoorarnav43@gmail.com>
2025-10-16 00:57:37 +0530

    Delete src/ADNLPProblems/trig.jl

--- src/ADNLPProblems/trig.jl
+++
@@ -1,9 +0,0 @@
-export trig
-
-function trig(::Val{:nlp}; n::Int = default_nvar, type::Type{T} = Float64, kwargs...) where {T}
-  function f(x; n = length(x))
-    return sum((sin(x[i]) + 0.5 * x[i])^2 for i = 1:n)
-  end
-  x0 = zeros(T, n)
-  return ADNLPModels.ADNLPModel(f, x0, name = "trig"; kwargs...)
-end
