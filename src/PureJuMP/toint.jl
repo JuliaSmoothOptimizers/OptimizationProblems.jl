76f9e9ba8b09d0547f0648814dac9b4b7b4d3fc0
Arnav Kapoor <kapoorarnav43@gmail.com>
2025-10-16 00:59:28 +0530

    Removing trig and toint

--- src/PureJuMP/toint.jl
+++
@@ -1,8 +0,0 @@
-export toint
-
-function toint(; n::Int = default_nvar)
-  nlp = Model()
-  @variable(nlp, x[1:n], start = 0.0)
-  @objective(nlp, Min, sum((cos(x[i]) - x[i]^2)^2 for i = 1:n))
-  return nlp
-end
--- src/PureJuMP/trig.jl
+++
@@ -1,8 +0,0 @@
-export trig
-
-function trig(; n::Int = default_nvar)
-  nlp = Model()
-  @variable(nlp, x[1:n], start = 0.0)
-  @objective(nlp, Min, sum((sin(x[i]) + 0.5 * x[i])^2 for i = 1:n))
-  return nlp
-end
