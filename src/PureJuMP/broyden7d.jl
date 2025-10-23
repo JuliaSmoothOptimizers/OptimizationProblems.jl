a14f598c7b69467d2a2e3e33380c99aad31afccf
Arnav Kapoor <kapoorarnav43@gmail.com>
2025-10-24 03:10:19 +0530

    Update src/PureJuMP/broyden7d.jl
    Co-authored-by: Tangi Migot <tangi.migot@gmail.com>

--- src/PureJuMP/broyden7d.jl
+++ src/PureJuMP/broyden7d.jl
@@ -5,7 +5,7 @@ function broyden7d(; n::Int = default_nvar)
   nh = div(n, 2)
   nlp = Model()
   @variable(nlp, x[1:n], start = -1.0)
-  @NLobjective(nlp, Min,
+  @objective(nlp, Min,
     abs((3 - 2 * x[1]) * x[1] - 0 - x[2] + 1)^p +
     sum(abs((3 - 2 * x[i]) * x[i] - x[i-1] - x[i+1] + 1)^p for i = 2:(n-1)) +
     abs((3 - 2 * x[n]) * x[n] - x[n-1] - 0 + 1)^p +
