d5ae2c98e66f0229837b7c6683e9559a9d331c1d
Arnav Kapoor <kapoorarnav43@gmail.com>
2025-10-26 00:56:01 +0530

    Update src/Meta/chainwoo.jl
    Co-authored-by: Tangi Migot <tangi.migot@gmail.com>

--- src/Meta/chainwoo.jl
+++ src/Meta/chainwoo.jl
@@ -23,4 +23,4 @@ get_chainwoo_nlin(; n::Integer = default_nvar, kwargs...) = 0
 get_chainwoo_nnln(; n::Integer = default_nvar, kwargs...) = 0
 get_chainwoo_nequ(; n::Integer = default_nvar, kwargs...) = 0
 get_chainwoo_nineq(; n::Integer = default_nvar, kwargs...) = 0
-get_chainwoo_nls_nequ(; n::Integer = default_nvar, kwargs...) = get_chainwoo_nvar(; n = n)
+get_chainwoo_nls_nequ(; n::Integer = default_nvar, kwargs...) = 4 * max(1, div(n, 4))
