d1c06f9480d0f3af1b2c7cd2d12a1ba18dd39158
Arnav Kapoor <kapoorarnav43@gmail.com>
2025-10-28 04:32:40 +0530

    easier implementation of the algorithm.

--- src/PureJuMP/nazareth.jl
+++ src/PureJuMP/nazareth.jl
@@ -12,33 +12,34 @@ export nazareth
 
 function nazareth(; n::Int = default_nvar, type::Type{T} = Float64) where {T}
   nh = div(n, 2)
-  neighbors = Vector{Vector{Int}}(undef, n)
-  a_row = Vector{Vector{T}}(undef, n)
-  b_row = Vector{Vector{T}}(undef, n)
-  for i in 1:n
-    lo = max(1, i - 2)
-    hi = min(n, i + 2)
-    neigh = collect(lo:hi)
-    j1 = i - nh
-    j2 = i + nh
-    for j in (j1, j2)
-      if 1 <= j <= n && !(j >= lo && j <= hi)
-        push!(neigh, j)
-      end
-    end
-    neighbors[i] = neigh
-    a_row[i] = [5 * (1 + mod(i, 5) + mod(j, 5)) for j in neigh]
-    b_row[i] = [(i + j) / 10 for j in neigh]
-  end
 
   nlp = Model()
-  @variable(nlp, x[1:n], start = one(T) / n)
+  @variable(nlp, x[1:n], start = one(T) / T(n))
 
   @objective(nlp, Min,
-    sum(
-      (n + i - sum(a_row[i][k] * sin(x[ neighbors[i][k] ]) + b_row[i][k] * cos(x[ neighbors[i][k] ]) for k in 1:length(neighbors[i])))^2
-      for i = 1:n
-    ) / n
+    sum((begin
+      lo = max(1, i - 2)
+      hi = min(n, i + 2)
+
+      sumtrig = 0
+      for j in lo:hi
+        aij = 5 * (1 + mod(i, 5) + mod(j, 5))
+        bij = (i + j) / 10
+        sumtrig += aij * sin(x[j]) + bij * cos(x[j])
+      end
+
+      j1 = i - nh
+      j2 = i + nh
+      for j in (j1, j2)
+        if 1 <= j <= n && (j < lo || j > hi)
+          aij = 5 * (1 + mod(i, 5) + mod(j, 5))
+          bij = (i + j) / 10
+          sumtrig += aij * sin(x[j]) + bij * cos(x[j])
+        end
+      end
+
+      (n + i - sumtrig)^2
+    end) for i = 1:n) / T(n)
   )
   return nlp
 end
