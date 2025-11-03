3eef3eb2cd6259c2985f8a4074b7d771635529e7
Arnav Kapoor <kapoorarnav43@gmail.com>
2025-11-03 23:58:55 +0530

    documentation

--- src/PureJuMP/variational.jl
+++ src/PureJuMP/variational.jl
@@ -1,3 +1,13 @@
+# Discretization of a variational problem
+#
+#   Problem 15 in
+#   L. Luksan, C. Matonoha and J. Vlcek
+#   Sparse Test Problems for Unconstrained Optimization,
+#   Technical Report 1064,
+#   Institute of Computer Science,
+#   Academy of Science of the Czech Republic
+#
+#   https://www.researchgate.net/publication/325314400_Sparse_Test_Problems_for_Unconstrained_Optimization
 export variational
 
 function variational(; n::Int = default_nvar, type::Type{T} = Float64, kwargs...) where {T}
