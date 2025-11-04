c956fb5dbd90bf810dc15c49616030452adf20c0
Arnav Kapoor <kapoorarnav43@gmail.com>
2025-11-04 09:24:30 +0530

    variational

--- src/ADNLPProblems/variational.jl
+++ src/ADNLPProblems/variational.jl
@@ -6,7 +6,7 @@ function variational(; use_nls::Bool = false, kwargs...)
 end
 
 function variational(::Val{:nlp}; n::Int = default_nvar, type::Type{T} = Float64, kwargs...) where {T}
-  h = one(T) / T(n + 1)
+  h = 1 // (n + 1)
 
   function f(x; n = length(x))
     S = eltype(x)
@@ -26,14 +26,7 @@ function variational(::Val{:nlp}; n::Int = default_nvar, type::Type{T} = Float64
       term1 += xi * (xi - xip1) / hS
     end
 
-    function diffquot(a, b)
-      d = b - a
-      if d == zero(d)
-        return exp(a)
-      else
-        return (exp(b) - exp(a)) / d
-      end
-    end
+    diffquot(a, b) = (b - a == zero(b - a)) ? exp(a) : (exp(b) - exp(a)) / (b - a)
 
     term2 = zero(S)
     @inbounds for j = 0:n
@@ -46,14 +39,14 @@ function variational(::Val{:nlp}; n::Int = default_nvar, type::Type{T} = Float64
 
   x0 = Vector{T}(undef, n)
   for i = 1:n
-    x0[i] = T(i) * h * (one(T) - T(i) * h)
+    x0[i] = i * h * (one(T) - i * h)
   end
 
   return ADNLPModels.ADNLPModel(f, x0, name = "variational", minimize = true; kwargs...)
 end
 
 function variational(::Val{:nls}; n::Int = default_nvar, type::Type{T} = Float64, kwargs...) where {T}
-  h = one(T) / T(n + 1)
+  h = 1 // (n + 1)
 
   function diffquot(a, b)
     d = b - a
--- src/PureJuMP/variational.jl
+++ src/PureJuMP/variational.jl
@@ -11,29 +11,29 @@
 export variational
 
 function variational(; n::Int = default_nvar, type::Type{T} = Float64, kwargs...) where {T}
-  h = one(T) / T(n + 1)
+  h = 1 // (n + 1)
   model = Model()
   # type-stable start matching ADNLPProblems
-  x0 = [T(i) * h * (one(T) - T(i) * h) for i = 1:n]
+  x0 = [i * h * (1 - i * h) for i = 1:n]
   @variable(model, x[i = 1:n], start = x0[i])
 
   # term1: interior sum plus last boundary term (avoid ternary inside macro)
   if n == 1
-    @expression(model, term1, x[1] * (x[1] - 0.0) / $(h))
+    @expression(model, term1, x[1] * (x[1] - 0.0) / h)
   else
-    @expression(model, term1_mid, sum(x[i] * (x[i] - x[i+1]) / $(h) for i = 1:n-1))
-    @expression(model, term1, term1_mid + x[n] * (x[n] - 0.0) / $(h))
+    @expression(model, term1_mid, sum(x[i] * (x[i] - x[i+1]) / h for i = 1:n-1))
+    @expression(model, term1, term1_mid + x[n] * (x[n] - 0.0) / h)
   end
 
-  # term2: build the boundary terms and interior sum separately to avoid
-  # invalid indexing in the NL macro (handle n==1 specially)
+  diffquot(a, b) = (b - a == zero(b - a)) ? exp(a) : (exp(b) - exp(a)) / (b - a)
+  JuMP.register(model, :diffquot, 2, diffquot; autodiff = true)
+
   if n == 1
-    @expression(model, term2, (exp(x[1]) - exp(0.0)) / (x[1] - 0.0) + (exp(0.0) - exp(x[1])) / (0.0 - x[1]))
+    @expression(model, term2, diffquot(x[1], 0.0) + diffquot(0.0, x[1]))
   else
-    @expression(model, term2_mid, sum((exp(x[j+1]) - exp(x[j])) / (x[j+1] - x[j]) for j = 1:n-1))
-    @expression(model, term2, (exp(x[1]) - exp(0.0)) / (x[1] - 0.0) + term2_mid + (exp(0.0) - exp(x[n])) / (0.0 - x[n]))
+    @expression(model, term2_mid, sum(diffquot(x[j+1], x[j]) for j = 1:n-1))
+    @expression(model, term2, diffquot(x[1], 0.0) + term2_mid + diffquot(0.0, x[n]))
   end
-
-  @objective(model, Min, $(T(2)) * (term1 + $(T(n)) * ($(h) / $(T(2))) * term2))
+  @objective(model, Min, 2 * (term1 + n * (h / 2) * term2))
   return model
 end
