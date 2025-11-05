94532e8fef08362f246c4f080f6762833a0ee3b8
Arnav Kapoor <kapoorarnav43@gmail.com>
2025-11-05 20:08:55 +0530

    JuliaFormatter

--- src/PureJuMP/boundary.jl
+++ src/PureJuMP/boundary.jl
@@ -15,8 +15,15 @@ function boundary(; n::Int = default_nvar, kwargs...)
   model = Model()
   x0 = [i * h * (1 - i * h) for i = 1:n]
   @variable(model, x[i = 1:n], start = x0[i])
-  @objective(model, Min,
-    sum((2 * x[i] - (i == 1 ? 0 : x[i-1]) - (i == n ? 0 : x[i+1])
-      + (h^2 / 2) * (x[i] + Float64(i) * h + 1)^3)^2 for i = 1:n))
+  @objective(
+    model,
+    Min,
+    sum(
+      (
+        2 * x[i] - (i == 1 ? 0 : x[i - 1]) - (i == n ? 0 : x[i + 1]) +
+        (h^2 / 2) * (x[i] + Float64(i) * h + 1)^3
+      )^2 for i = 1:n
+    )
+  )
   return model
 end
