4b5ba91dd1a55482a859b78531d4690abbdb0ffd
Arnav Kapoor <kapoorarnav43@gmail.com>
2025-11-07 13:04:13 +0530

    variational

--- src/ADNLPProblems/variational.jl
+++ src/ADNLPProblems/variational.jl
@@ -2,40 +2,23 @@ export variational
 
 function variational(; n::Int = default_nvar, type::Type{T} = Float64, kwargs...) where {T}
   h = 1 // (n + 1)
+  x0 = [convert(T, i * h * (1 - i * h)) for i = 1:n]
 
-  x0 = Vector{T}(undef, n)
-  for i = 1:n
-    x0[i] = i * h * (one(T) - i * h)
-  end
+  f = x -> begin
+    xext = vcat(zero(eltype(x)), x, zero(eltype(x)))
 
-  f = (x; n = length(x)) -> begin
-    S = eltype(x)
-    hS = convert(S, h)
+    term1 = sum(x[i] * (x[i] - xext[i + 2]) / convert(eltype(x), h) for i = 1:n)
+    term2 = sum(
+      if eltype(x) <: AbstractFloat
+        d = xext[j + 2] - xext[j + 1]
+        abs(d) <= eps(eltype(x)) ? exp(xext[j + 1]) : (exp(xext[j + 2]) - exp(xext[j + 1])) / d
+      else
+        d = xext[j + 2] - xext[j + 1]
+        exp(xext[j + 1]) * expm1(d) / d
+      end for j = 0:n
+    )
 
-    xext = Vector{S}(undef, n + 2)
-    xext[1] = zero(S)
-    @inbounds for i = 1:n
-      xext[i + 1] = x[i]
-    end
-    xext[n + 2] = zero(S)
-
-    term1 = zero(S)
-    @inbounds for i = 1:n
-      xi = x[i]
-      xip1 = xext[i + 2]
-      term1 += xi * (xi - xip1) / hS
-    end
-
-    term2 = zero(S)
-    @inbounds for j = 0:n
-      a = xext[j + 1]
-      b = xext[j + 2]
-      d = b - a
-      Pd = one(d) + d / 2 + d^2 / 6 + d^3 / 24 + d^4 / 120
-      term = exp(a) * Pd
-      term2 += term
-    end
-    return 2 * (term1 + n * (hS / 2) * term2)
+    return 2 * (term1 + n * (convert(eltype(x), h) / 2) * term2)
   end
 
   return ADNLPModels.ADNLPModel(f, x0, name = "variational", minimize = true; kwargs...)
--- src/PureJuMP/variational.jl
+++ src/PureJuMP/variational.jl
@@ -12,53 +12,22 @@ export variational
 
 function variational(; n::Int = default_nvar, kwargs...)
   h = 1 // (n + 1)
+  x0 = [i * h * (1.0 - i * h) for i = 1:n]
   model = Model()
-  x0 = Vector{Float64}(undef, n)
-  for i = 1:n
-    x0[i] = i * h * (1.0 - i * h)
-  end
   @variable(model, x[i = 1:n], start = x0[i])
 
-  if n == 1
-    @objective(
-      model,
-      Min,
-      2 * (
-        x[1] * (x[1] - 0.0) / h +
-        n *
-        (h / 2) *
-        (
-          exp(0) * (1 + (x[1] - 0)/2 + (x[1] - 0)^2/6 + (x[1] - 0)^3/24 + (x[1] - 0)^4/120) +
-          exp(x[1]) * (1 + (0 - x[1])/2 + (0 - x[1])^2/6 + (0 - x[1])^3/24 + (0 - x[1])^4/120)
-        )
+  @objective(
+    model,
+    Min,
+    2 * (
+      sum(x[i] * (x[i] - (i == n ? 0 : x[i + 1])) / h for i = 1:n) +
+      n * (h / 2) * (
+        (exp(x[1]) - 1) / x[1] +
+        sum((exp(x[j + 1]) - exp(x[j])) / (x[j + 1] - x[j]) for j = 1:(n - 1)) +
+        (1 - exp(x[n])) / (-x[n])
       )
     )
-  else
-    @objective(
-      model,
-      Min,
-      2 * (
-        (sum(x[i] * (x[i] - x[i + 1]) / h for i = 1:(n - 1)) + x[n] * (x[n] - 0) / h) +
-        n *
-        (h / 2) *
-        (
-          exp(0) *
-          (1 + (x[1] - 0) / 2 + (x[1] - 0)^2 / 6 + (x[1] - 0)^3 / 24 + (x[1] - 0)^4 / 120) +
-          sum(
-            exp(x[j]) * (
-              1 +
-              (x[j + 1] - x[j]) / 2 +
-              (x[j + 1] - x[j])^2 / 6 +
-              (x[j + 1] - x[j])^3 / 24 +
-              (x[j + 1] - x[j])^4 / 120
-            ) for j = 1:(n - 1)
-          ) +
-          exp(x[n]) *
-          (1 + (0 - x[n]) / 2 + (0 - x[n])^2 / 6 + (0 - x[n])^3 / 24 + (0 - x[n])^4 / 120)
-        )
-      )
-    )
-  end
+  )
 
   return model
 end
