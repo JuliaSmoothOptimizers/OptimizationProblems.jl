7af470ef37b09063f2f48a8b71f57fb7d787a699
Arnav Kapoor <kapoorarnav43@gmail.com>
2025-11-12 00:54:00 +0530

    fixing float32 issue in variational problems

--- src/PureJuMP/variational.jl
+++ src/PureJuMP/variational.jl
@@ -11,25 +11,25 @@
 export variational
 
 function variational(; n::Int = default_nvar, kwargs...)
-	h = 1 // (n + 1)
-	x0 = [(i * h) * (1 - i * h) for i ∈ 1:n]
-	model = Model()
-	@variable(model, x[i=1:n], start = x0[i])
+  h = 1 // (n + 1)
+  x0 = [convert(T, (i * h) * (1 - i * h)) for i = 1:n]
+  model = Model()
+  @variable(model, x[i = 1:n], start = x0[i])
 
-	@objective(
-		model,
-		Min,
-		2 * (
-			(sum(x[i] * (x[i] - x[i+1]) / h for i ∈ 1:(n-1)) + x[n] * x[n] / h) +
-			n *
-			(h / 2) *
-			(
-				(exp(x[1]) - exp(0)) / (x[1] - 0) +
-				sum((exp(x[j+1]) - exp(x[j])) / (x[j+1] - x[j]) for j ∈ 1:(n-1)) +
-				(exp(0) - exp(x[n])) / (0 - x[n])
-			)
-		)
-	)
+  @objective(
+    model,
+    Min,
+    2 * (
+      (sum(x[i] * (x[i] - x[i + 1]) / h for i ∈ 1:(n - 1)) + x[n] * x[n] / h) +
+      n *
+      (h / 2) *
+      (
+        (exp(x[1]) - exp(0)) / (x[1] - 0) +
+        sum((exp(x[j + 1]) - exp(x[j])) / (x[j + 1] - x[j]) for j ∈ 1:(n - 1)) +
+        (exp(0) - exp(x[n])) / (0 - x[n])
+      )
+    )
+  )
 
-	return model
+  return model
 end
