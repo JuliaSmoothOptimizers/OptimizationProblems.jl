3d76a65acd68901ac753eeaed0c45610442069a1
Tangi Migot <tangi.migot@gmail.com>
2025-11-12 18:37:27 -0500

    Update src/ADNLPProblems/variational.jl

--- src/ADNLPProblems/variational.jl
+++ src/ADNLPProblems/variational.jl
@@ -5,22 +5,22 @@ function variational(; n::Int = default_nvar, type::Type{T} = Float64, kwargs...
   x0 = T[(i * h) * (1 - i * h) for i = 1:n]
 
 function f(x; n = length(x))
-  T = eltype(x)
+  Ti = eltype(x)
 
-  term1 = zero(T)
-  term2 = zero(T)
+  term1 = zero(Ti)
+  term2 = zero(Ti)
   @inbounds for k = 1:n
     xi  = x[k]
-    xip = (k < n) ? x[k + 1] : zero(T)
+    xip = (k < n) ? x[k + 1] : zero(Ti)
 
     term1 += xi * (xi - xip) / h
 
-    a_prev = (k == 1) ? zero(T) : x[k - 1]
+    a_prev = (k == 1) ? zero(Ti) : x[k - 1]
     b_prev = xi
     term2 += (exp(b_prev) - exp(a_prev)) / (b_prev - a_prev)
 
     if k == n
-      a_last, b_last = xi, zero(T)
+      a_last, b_last = xi, zero(Ti)
       term2 += (exp(b_last) - exp(a_last)) / (b_last - a_last)
     end
   end
