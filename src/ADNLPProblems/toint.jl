8b80602410186f8fe7d356a9fe90f95cacffb2ab
Arnav Kapoor <kapoorarnav43@gmail.com>
2025-11-19 02:11:27 +0530

    fixing Float64 issue in toint and trig problems

--- src/ADNLPProblems/toint.jl
+++ src/ADNLPProblems/toint.jl
@@ -1,21 +1,21 @@
 export toint
 
 function toint(; n::Int = default_nvar, type::Type{T} = Float64, kwargs...) where {T}
-  function f(x; n = length(x))
-    s = zero(T)
-    for i = 1:n
-      ci = 1 + (i // 10)
+	function f(x; n = length(x))
+		s = zero(T)
+		for i ∈ 1:n
+			ci = 1 + (i // 10)
 
-      for j = max(1, i - 2):min(n, i + 2)
-        aij = 5 * (1 + mod(i, 5) + mod(j, 5))
-        bij = (i + j) // 10
-        cj = 1 + (j // 10)
-        s += aij * sin(bij + ci * x[i] + cj * x[j])
-      end
-    end
-    return s / n
-  end
+			for j ∈ max(1, i-2):min(n, i+2)
+				aij = 5 * (1 + mod(i, 5) + mod(j, 5))
+				bij = (i + j) // 10
+				cj = (1 + j) // 10
+				s += aij * sin(bij + ci * x[i] + cj * x[j])
+			end
+		end
+		return s / n
+	end
 
-  x0 = fill(one(T), n)
-  return ADNLPModels.ADNLPModel(f, x0, name = "toint"; kwargs...)
+	x0 = fill(one(T), n)
+	return ADNLPModels.ADNLPModel(f, x0, name = "toint"; kwargs...)
 end
