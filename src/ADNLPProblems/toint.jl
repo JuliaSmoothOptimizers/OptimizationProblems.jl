84f4991266eeb3ff04ad0889a133123966d43fe3
Arnav Kapoor <kapoorarnav43@gmail.com>
2025-11-19 00:16:39 +0530

    fixing remaining mentions of xi variable

--- src/ADNLPProblems/toint.jl
+++ src/ADNLPProblems/toint.jl
@@ -10,7 +10,7 @@ function toint(; n::Int = default_nvar, type::Type{T} = Float64, kwargs...) wher
         aij = 5 * (1 + mod(i, 5) + mod(j, 5))
         bij = (i + j) / 10
         cj = (1 + j) / 10
-        s += aij * sin(bij + ci * xi + cj * x[j])
+        s += aij * sin(bij + ci * x[i] + cj * x[j])
       end
 
       if iseven(n)
@@ -19,7 +19,7 @@ function toint(; n::Int = default_nvar, type::Type{T} = Float64, kwargs...) wher
           aij = 5 * (1 + mod(i, 5) + mod(j, 5))
           bij = (i + j) / 10
           cj = (1 + j) / 10
-          s += aij * sin(bij + ci * xi + cj * x[j])
+          s += aij * sin(bij + ci * x[i] + cj * x[j])
         end
       end
     end
