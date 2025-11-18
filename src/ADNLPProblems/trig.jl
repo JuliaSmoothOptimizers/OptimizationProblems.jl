04c8ad10aa9bb47e5c61524586b15cc4409bb922
Arnav Kapoor <kapoorarnav43@gmail.com>
2025-11-18 15:51:11 +0530

    Update trig.jl

--- src/ADNLPProblems/trig.jl
+++ src/ADNLPProblems/trig.jl
@@ -6,21 +6,21 @@ function trig(; n::Int = default_nvar, type::Type{T} = Float64, kwargs...) where
     s = zero(T)
     for i = 1:n_local
       xi = x[i]
-      term = T(i) * (one(T) - cos(xi))
+      term = i * (1 - cos(xi))
 
       jlo = max(1, i - 2)
       jhi = min(n_local, i + 2)
       for j = jlo:jhi
-        aij = 5 * (1 + (mod(i, 5)) + (mod(j, 5)))
-        bij = (i + j) // 10
+        aij = 5 * (1 + mod(i, 5) + mod(j, 5))
+        bij = (i + j) / 10
         term += aij * sin(x[j]) + bij * cos(x[j])
       end
 
       if iseven(n_local)
         j = i + (n_local ÷ 2)
         if 1 <= j <= n_local
-          aij = 5 * (1 + (mod(i, 5)) + (mod(j, 5)))
-          bij = (i + j) // 10
+          aij = 5 * (1 + mod(i, 5) + mod(j, 5))
+          bij = (i + j) / 10
           term += aij * sin(x[j]) + bij * cos(x[j])
         end
       end
