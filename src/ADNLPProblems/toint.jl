4b619623efae903bcbcf6b9229811b7b6252dbd8
Arnav Kapoor <kapoorarnav43@gmail.com>
2025-12-20 10:03:32 +0530

    fixing AD and JuMP inconsistency in toint.jl

--- src/ADNLPProblems/toint.jl
+++ src/ADNLPProblems/toint.jl
@@ -4,13 +4,13 @@ function toint(; n::Int = default_nvar, type::Type{T} = Float64, kwargs...) wher
 	function f(x; n = length(x))
 		s = zero(T)
 		for i ∈ 1:n
-			ci = 1 + (i // 10)
+			ci = 1 + i / 10
 			jmin = max(1, i - 2)
 			jmax = min(n, i + 2)
 			for j ∈ jmin:jmax
 				aij = 5 * (1 + mod(i, 5) + mod(j, 5))
-				bij = (i + j) // 10
-				cj = (1 + j) // 10
+				bij = (i + j) / 10
+				cj = 1 + j / 10
 				s += aij * sin(bij + ci * x[i] + cj * x[j])
 			end
 
@@ -19,15 +19,15 @@ function toint(; n::Int = default_nvar, type::Type{T} = Float64, kwargs...) wher
 				j1 = i + half
 				if 1 <= j1 <= n && (j1 < jmin || j1 > jmax)
 					aij = 5 * (1 + mod(i, 5) + mod(j1, 5))
-					bij = (i + j1) // 10
-					cj = (1 + j1) // 10
+					bij = (i + j1) / 10
+					cj = 1 + j1 / 10
 					s += aij * sin(bij + ci * x[i] + cj * x[j1])
 				end
 				j2 = i - half
 				if 1 <= j2 <= n && j2 != j1 && (j2 < jmin || j2 > jmax)
 					aij = 5 * (1 + mod(i, 5) + mod(j2, 5))
-					bij = (i + j2) // 10
-					cj = (1 + j2) // 10
+					bij = (i + j2) / 10
+					cj = 1 + j2 / 10
 					s += aij * sin(bij + ci * x[i] + cj * x[j2])
 				end
 			end
