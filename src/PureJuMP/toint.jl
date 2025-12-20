3ad295438503e9e1a153aefd3e5f58aec9ad4df8
Arnav Kapoor <kapoorarnav43@gmail.com>
2025-12-20 10:38:33 +0530

    fixing AD and JuMP incompatibility in toint.jl - 2

--- src/PureJuMP/toint.jl
+++ src/PureJuMP/toint.jl
@@ -19,15 +19,15 @@ function toint(args...; n::Int = default_nvar, kwargs...)
     model,
     Min,
     (1 / n) * sum(begin
-      ci = 1 + i / 10
+      ci = 1 + (i // 10)
       s = zero(Float64)
 
       jmin = max(1, i - 2)
       jmax = min(n, i + 2)
       for j in jmin:jmax
         aij = 5 * (1 + mod(i, 5) + mod(j, 5))
-        bij = (i + j) / 10
-        cj = 1 + j / 10
+        bij = (i + j) // 10
+        cj = (1 + j) // 10
         s += aij * sin(bij + ci * x[i] + cj * x[j])
       end
 
@@ -36,15 +36,15 @@ function toint(args...; n::Int = default_nvar, kwargs...)
         j1 = i + half
         if 1 <= j1 <= n && (j1 < jmin || j1 > jmax)
           aij = 5 * (1 + mod(i, 5) + mod(j1, 5))
-          bij = (i + j1) / 10
-          cj = 1 + j1 / 10
+          bij = (i + j1) // 10
+          cj = (1 + j1) // 10
           s += aij * sin(bij + ci * x[i] + cj * x[j1])
         end
         j2 = i - half
         if 1 <= j2 <= n && j2 != j1 && (j2 < jmin || j2 > jmax)
           aij = 5 * (1 + mod(i, 5) + mod(j2, 5))
-          bij = (i + j2) / 10
-          cj = 1 + j2 / 10
+          bij = (i + j2) // 10
+          cj = (1 + j2) // 10
           s += aij * sin(bij + ci * x[i] + cj * x[j2])
         end
       end
