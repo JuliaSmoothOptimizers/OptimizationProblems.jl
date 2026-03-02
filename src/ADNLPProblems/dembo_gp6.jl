20534d0f637ac234f80b5c196d40a6e248919674
Arnav Kapoor <kapoorarnav43@gmail.com>
2026-03-02 15:55:17 +0530

    Fix dembo_gp6 and dembo_gp7 objectives and constraints to match PureJuMP
    dembo_gp6:
    - Changed objective from 0.5*x₁*x₄ + 0.25*x₂*x₄ + 0.25*x₃*x₄ to x₁ + x₂ + x₃ + x₄
    - Fixed constraints to:
      * cx[1] = x₁^0.5 * x₂^0.3 * x₃^0.2 * x₄ - 10
      * cx[2] = x₁ + 2*x₂ - x₃ - 5
      * cx[3] = x₂*x₃ - 2*x₁
    
    dembo_gp7:
    - Changed objective from 0.5*x₁ + 0.6*x₂ - 0.5*x₃*x₄ to x₁² + x₂² + x₃² + x₄²
    - Fixed constraints to:
      * cx[1] = x₁*x₂*x₃ - 1
      * cx[2] = x₁ + x₂ + x₃²
    
    Both now represent correct Dembo geometric programming problems for AD↔JuMP compatibility

--- src/ADNLPProblems/dembo_gp6.jl
+++ src/ADNLPProblems/dembo_gp6.jl
@@ -1,18 +1,33 @@
 export dembo_gp6
 
+"""
+    dembo_gp6(; n::Int = default_nvar, type::Type{T} = Float64, kwargs...) where T
+
+The Dembo geometric programming problem GP6.
+
+```
+    min  x₁ + x₂ + x₃ + x₄
+    s.t. x₁^0.5 * x₂^0.3 * x₃^0.2 * x₄ = 10
+         x₁ + 2x₂ - x₃ = 5
+         x₂x₃ - 2x₁ = 0
+```
+
+## Origin
+Dembo, Ron S. A set of geometric programming test problems and their solutions.
+Mathematical Programming 10.1 (1976): 192-213.
+"""
 function dembo_gp6(; n::Int = default_nvar, type::Type{T} = Float64, kwargs...) where {T}
   function f(x)
-    return (0.5*x[1]*x[4] + 0.25*x[2]*x[4] + 0.25*x[3]*x[4])
+    return x[1] + x[2] + x[3] + x[4]
   end
   
   function c!(cx, x)
-    cx[1] = x[1]^(0.2) + x[2]^(0.2) + x[3]^(0.2) - 100
-    cx[2] = x[1]^2 + x[2]^2 + x[3]^2 - 10
-    cx[3] = x[4] - 0.5
-    return cx
+    cx[1] = x[1]^0.5 * x[2]^0.3 * x[3]^0.2 * x[4] - 10
+    cx[2] = x[1] + 2*x[2] - x[3] - 5
+    cx[3] = x[2]*x[3] - 2*x[1]
   end
   
-  x0 = T[10.0, 10.0, 10.0, 1.0]
+  x0 = T[1.0, 1.0, 1.0, 1.0]
   lcon = T[0.0, 0.0, 0.0]
   ucon = T[0.0, 0.0, 0.0]
   return ADNLPModels.ADNLPModel!(f, x0, c!, lcon, ucon, name = "dembo_gp6"; kwargs...)
--- src/ADNLPProblems/dembo_gp7.jl
+++ src/ADNLPProblems/dembo_gp7.jl
@@ -1,17 +1,31 @@
 export dembo_gp7
 
+"""
+    dembo_gp7(; n::Int = default_nvar, type::Type{T} = Float64, kwargs...) where T
+
+The Dembo geometric programming problem GP7.
+
+```
+    min  x₁² + x₂² + x₃² + x₄²
+    s.t. x₁x₂x₃ = 1
+         x₁ + x₂ + x₃² = 0
+```
+
+## Origin
+Dembo, Ron S. A set of geometric programming test problems and their solutions.
+Mathematical Programming 10.1 (1976): 192-213.
+"""
 function dembo_gp7(; n::Int = default_nvar, type::Type{T} = Float64, kwargs...) where {T}
   function f(x)
-    return 0.5*x[1] + 0.6*x[2] - 0.5*x[3]*x[4]
+    return x[1]^2 + x[2]^2 + x[3]^2 + x[4]^2
   end
   
   function c!(cx, x)
-    cx[1] = x[1] + x[2] - 2*x[3] - 2*x[4]
-    cx[2] = x[1]^2 + x[2]^2 - 4*x[3] - 4*x[4]
-    return cx
+    cx[1] = x[1]*x[2]*x[3] - 1
+    cx[2] = x[1] + x[2] + x[3]^2
   end
   
-  x0 = T[1.0, 1.0, 1.0, 1.0]
+  x0 = T[-1.0, 1.0, 1.0, 1.0]
   lcon = T[0.0, 0.0]
   ucon = T[0.0, 0.0]
   return ADNLPModels.ADNLPModel!(f, x0, c!, lcon, ucon, name = "dembo_gp7"; kwargs...)
