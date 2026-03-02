24f0c4c8fdfb0ca9e2b5468ad2cf3e2c26beb7fe
Arnav Kapoor <kapoorarnav43@gmail.com>
2026-03-02 15:51:58 +0530

    Fix dembo_gp3: align ADNLPProblems with PureJuMP formulation
    - Changed objective from 0.5*x₁*x₄ + 0.5*x₂*x₄ + x₃ to x₁ + x₂/x₁ + x₃/x₁² + x₄
    - Changed constraints to match Dembo's GP3:
      * cx[1] = x₃ - x₁³
      * cx[2] = x₂ - 2x₁²
      * cx[3] = x₃ + 1/x₄ - 2
    - Updated initial point and constraint bounds for consistency
    - Now represents the same problem for AD↔JuMP compatibility

--- src/ADNLPProblems/dembo_gp3.jl
+++ src/ADNLPProblems/dembo_gp3.jl
@@ -1,18 +1,33 @@
 export dembo_gp3
 
+"""
+    dembo_gp3(; n::Int = default_nvar, type::Type{T} = Float64, kwargs...) where T
+
+The Dembo geometric programming problem GP3.
+
+```
+    min  x₁ + x₂/x₁ + x₃/x₁² + x₄
+    s.t. x₃ - x₁³ = 0
+         x₂ - 2x₁² = 0
+         x₃ + 1/x₄ = 2
+```
+
+## Origin
+Dembo, Ron S. A set of geometric programming test problems and their solutions.
+Mathematical Programming 10.1 (1976): 192-213.
+"""
 function dembo_gp3(; n::Int = default_nvar, type::Type{T} = Float64, kwargs...) where {T}
   function f(x)
-    return 0.5 * x[1] * x[4] + 0.5 * x[2] * x[4] + x[3]
+    return x[1] + x[2]/x[1] + x[3]/x[1]^2 + x[4]
   end
   
   function c!(cx, x)
-    cx[1] = x[1]^3 + x[2]^3 - 1
-    cx[2] = x[1]^2 * x[3] - 1
-    cx[3] = x[4]^2 - 1
-    return cx
+    cx[1] = x[3] - x[1]^3
+    cx[2] = x[2] - 2*x[1]^2
+    cx[3] = x[3] + 1/x[4] - 2
   end
   
-  x0 = T[0.5, 0.5, 0.5, 1.0]
+  x0 = T[1.0, 2.0, 1.0, 1.0]
   lcon = T[0.0, 0.0, 0.0]
   ucon = T[0.0, 0.0, 0.0]
   return ADNLPModels.ADNLPModel!(f, x0, c!, lcon, ucon, name = "dembo_gp3"; kwargs...)
