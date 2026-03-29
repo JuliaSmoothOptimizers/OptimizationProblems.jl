b0f84428adc9da33d93752dea13f1c389b5cb09b
tmigot <tangi.migot@gmail.com>
2026-03-29 13:29:05 -0400

    up

--- src/Meta/aircrfta.jl
+++ src/Meta/aircrfta.jl
@@ -16,6 +16,7 @@ aircrfta_meta = Dict(
   :is_feasible => true,
   :defined_everywhere => missing,
   :origin => :unknown,
+  :url => "https://www.osti.gov/biblio/6449249",
   :notes => raw"""
 Converted in Julia from https://github.com/mpf/Optimization-Test-Problems
 
@@ -31,14 +32,23 @@ copies and that the copyright notice and this
 permission notice appear in all supporting documentation.
 """,
   :origin_notes => raw"""
-SIF input: Ph. Toint, Dec 1989. classification NOR2-RN-8-5
-""",
-  :reference => raw"""
+SIF input: Ph. Toint, Dec 1989. classification NOR2-RN-8-5.
 Problem 9,
 J.J. More, A collection of nonlinear model problems,
 Proceedings of the AMS-SIAM Summer Seminar on the Computational.
 Solution of Nonlinear Systems of Equations, Colorado, 1988.
 Argonne National Laboratory MCS-P60-0289, 1989.
+""",
+  :reference => raw"""
+@article{more1990collection,
+  title={A collection of nonlinear model problems. Computational Solution of Nonlinear Systems of Equations},
+  author={Mor{\'e}, Jorge J.},
+  journal={Lectures in Applied Mathematics},
+  volume={26},
+  pages={723--762},
+  year={1990},
+  publisher={American Mathematical Society}
+}
 """,
 )
 get_aircrfta_nvar(; n::Integer = default_nvar, kwargs...) = 8
