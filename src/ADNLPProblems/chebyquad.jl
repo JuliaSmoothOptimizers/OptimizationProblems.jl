4c847c5fb793167b630177c4cb80d2c7ace6e26f
arnavk23 <kapoorarnav43@gmail.com>
2026-04-06 20:59:51 +0530

    removing extra variables from the chebyquad problem

--- src/ADNLPProblems/chebyquad.jl
+++ src/ADNLPProblems/chebyquad.jl
@@ -16,8 +16,7 @@ function Cheby(xj, i)
         return (-1)^i * cosh(i * acosh(-xj))
     else
         # Clamp only for acos to guard against tiny floating-point/AD excursions.
-        xj_clamped = min(max(xj, -1), 1)
-        return cos(i * acos(xj_clamped))
+      return cos(i * acos(min(max(xj, -1), 1)))
     end
 end
 
@@ -31,20 +30,17 @@ function chebyquad(
 ) where {T}
   m = max(m, n)
   function f(x; n = length(x), m = m, chebyshev = chebyshev)
-    inv_n = one(T) / n
-    half = one(T) / 2
-    return half * sum(
-      (inv_n * sum(chebyshev(x[j], 2i) for j = 1:n) + one(T) / ((2i)^2 - 1))^2 for
+    return (one(T) / 2) * sum(
+      ((one(T) / n) * sum(chebyshev(x[j], 2i) for j = 1:n) + one(T) / ((2i)^2 - 1))^2 for
       i = 1:div(m, 2)
     ) +
-           half * sum(
-      (inv_n * sum(chebyshev(x[j], 2i - 1) for j = 1:n))^2 for i = 1:div(m + 1, 2)
+           (one(T) / 2) * sum(
+      ((one(T) / n) * sum(chebyshev(x[j], 2i - 1) for j = 1:n))^2 for i = 1:div(m + 1, 2)
     )
   end
-  step = one(T) / (n + one(T))
   x0 = Vector{T}(undef, n)
   for j = 1:n
-    x0[j] = j * step
+    x0[j] = j * one(T) / (n + one(T))
   end
   return ADNLPModels.ADNLPModel(f, x0, name = "chebyquad"; kwargs...)
 end
@@ -59,20 +55,18 @@ function chebyquad(
 ) where {T}
   m = max(m, n)
   function F!(r, x; n = length(x), m = length(r), chebyshev = chebyshev)
-    inv_n = one(T) / n
     for i = 1:div(m, 2)
-      r[2i] = inv_n * sum(chebyshev(x[j], 2i) for j = 1:n) + one(T) / ((2i)^2 - 1)
-      r[2i - 1] = inv_n * sum(chebyshev(x[j], 2i - 1) for j = 1:n)
+      r[2i] = (one(T) / n) * sum(chebyshev(x[j], 2i) for j = 1:n) + one(T) / ((2i)^2 - 1)
+      r[2i - 1] = (one(T) / n) * sum(chebyshev(x[j], 2i - 1) for j = 1:n)
     end
     if mod(m, 2) == 1
-      r[m] = inv_n * sum(chebyshev(x[j], m) for j = 1:n)
+      r[m] = (one(T) / n) * sum(chebyshev(x[j], m) for j = 1:n)
     end
     return r
   end
-  step = one(T) / (n + one(T))
   x0 = Vector{T}(undef, n)
   for j = 1:n
-    x0[j] = j * step
+    x0[j] = j * one(T) / (n + one(T))
   end
   return ADNLPModels.ADNLSModel!(F!, x0, m, name = "chebyquad-nls"; kwargs...)
 end
