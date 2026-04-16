1f388f0501fc7ddd825d00e2795863f85efa05d7
tmigot <tangi.migot@gmail.com>
2026-04-16 12:37:16 -0400

    do not move license

--- src/PureJuMP/aircrfta.jl
+++ src/PureJuMP/aircrfta.jl
@@ -1,3 +1,17 @@
+#
+# Converted in Julia from https://github.com/mpf/Optimization-Test-Problems
+#
+# AMPL Model by Hande Y. Benson
+#
+# Copyright (C) 2001 Princeton University
+# All Rights Reserved
+#
+# Permission to use, copy, modify, and distribute this software and
+# its documentation for any purpose and without fee is hereby
+# granted, provided that the above copyright notice appear in all
+# copies and that the copyright notice and this
+# permission notice appear in all supporting documentation.
+
 export aircrfta
 
 function aircrfta(; n::Int = default_nvar, kwargs...)
