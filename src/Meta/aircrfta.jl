d6f08913259f18325194ab3bee041c598b49a924
tmigot <tangi.migot@gmail.com>
2026-04-16 12:37:16 -0400

    do not move license

--- src/Meta/aircrfta.jl
+++ src/Meta/aircrfta.jl
@@ -18,18 +18,7 @@ aircrfta_meta = Dict(
   :origin => :unknown,
   :url => "https://www.osti.gov/biblio/6449249",
   :notes => raw"""
-Converted in Julia from https://github.com/mpf/Optimization-Test-Problems
 
-AMPL Model by Hande Y. Benson
-
-Copyright (C) 2001 Princeton University
-All Rights Reserved
-
-Permission to use, copy, modify, and distribute this software and
-its documentation for any purpose and without fee is hereby
-granted, provided that the above copyright notice appear in all
-copies and that the copyright notice and this
-permission notice appear in all supporting documentation.
 """,
   :origin_notes => raw"""
 SIF input: Ph. Toint, Dec 1989. classification NOR2-RN-8-5.
