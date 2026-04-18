b59e1ba171dc3a18b0372adf21e8757a4d68136a
arnavk23 <kapoorarnav43@gmail.com>
2026-04-19 01:26:59 +0530

    adding copilot suggestions to fix dimension warnings in ADNLPProblems and PureJuMP

--- src/OptimizationProblems.jl
+++ src/OptimizationProblems.jl
@@ -2,6 +2,22 @@ module OptimizationProblems
 
 using DataFrames
 
+_adjust_nvar_warn_message(problem_name, n_orig, n) =
+  string(problem_name, ": number of variables adjusted from ", n_orig, " to ", n)
+
+"""
+    @adjust_nvar_warn(problem_name, n_orig, n)
+
+Issue a warning if the number of variables was adjusted, showing both original and adjusted values.
+"""
+macro adjust_nvar_warn(problem_name, n_orig, n)
+  return quote
+    local _n_orig = $(esc(n_orig))
+    local _n = $(esc(n))
+    (_n == _n_orig) || @warn(_adjust_nvar_warn_message($(esc(problem_name)), _n_orig, _n))
+  end
+end
+
 include("ADNLPProblems/ADNLPProblems.jl")
 include("PureJuMP/PureJuMP.jl")
 
